4408d762b59cec855881ef1eb6f71eaef041aa19
Bart Bilos <squantmuts@gmail.com>
2019-08-25 21:56:20 +0200

    fixed typo

--- inc/nxp/LPC8XX/LPC8XX_wwdt.h
+++ inc/nxp/LPC8XX/LPC8XX_wwdt.h
@@ -36,7 +36,7 @@ LPC800 series common windowed watchdog functions
 typedef struct {
     volatile uint32_t  MOD;             /* basic mode and status register */
     volatile uint32_t  TC;              /* time-out value register */
-    volatile  uint32_t  FEED;           /* Writing 0xAA followed by 0x55 reloads the Watchdog timer */
+    volatile uint32_t  FEED;           /* Writing 0xAA followed by 0x55 reloads the Watchdog timer */
     volatile const  uint32_t  TV;       /* current value of the Watchdog timer */
     volatile const  uint32_t  RESERVED0;
     volatile uint32_t  WARNINT;         /* contains the Watchdog warning interrupt value */
