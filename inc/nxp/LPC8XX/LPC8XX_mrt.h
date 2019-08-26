30bf41d945ef6dc95ee20b46b2cbc7d0490770f4
Bart Bilos <squantmuts@gmail.com>
2019-08-26 18:11:10 +0200

    imported multi rate timer peripheral

--- inc/nxp/LPC8XX/LPC8XX_mrt.h
+++ inc/nxp/LPC8XX/LPC8XX_mrt.h
@@ -31,50 +31,41 @@ functions.
 #ifndef LPC8XX_MRT_H
 #define LPC8XX_MRT_H
 
-/**
- * @brief LPC8xx MRT chip configuration
- */
+/* LPC8xx MRT chip configuration */
+// TODO: move to controller definitions?
 #define MRT_CHANNELS_NUM      (4)
 #define MRT_NO_IDLE_CHANNEL   (0x40)
 
-/**
- * @brief MRT register block structure
- */
+/* MRT register block structure */
 typedef struct {
-	__IO uint32_t INTVAL;	/*!< Timer interval register */
-	__O  uint32_t TIMER;	/*!< Timer register */
-	__IO uint32_t CTRL;		/*!< Timer control register */
-	__IO uint32_t STAT;		/*!< Timer status register */
+    volatile uint32_t INTVAL;    /*!< Timer interval register */
+    volatile uint32_t TIMER;    /*!< Timer register */
+    volatile uint32_t CTRL;        /*!< Timer control register */
+    volatile uint32_t STAT;        /*!< Timer status register */
 } LPC_MRT_CH_T;
 
-/**
- * @brief MRT register block structure
- */
+/* MRT register block structure */
 typedef struct {
-	LPC_MRT_CH_T CHANNEL[MRT_CHANNELS_NUM];
-	uint32_t unused[45];
-	__O  uint32_t IDLE_CH;
-	__IO uint32_t IRQ_FLAG;
+    LPC_MRT_CH_T CHANNEL[MRT_CHANNELS_NUM];
+    uint32_t unused[45];
+    volatile uint32_t IDLE_CH;
+    volatile uint32_t IRQ_FLAG;
 } LPC_MRT_T;
 
 /* Reserved bits masks for registers */
+// TODO: move to controller definitions?
 #define MRT_CTRL_RESERVED   (~7)
 #define MRT_STAT_RESERVED   (~3)
 
-/**
- * @brief MRT Interrupt Modes enum
- */
+/* MRT Interrupt Modes enum */
 typedef enum MRT_MODE {
-	MRT_MODE_REPEAT =  (0 << 1),	/*!< MRT Repeat interrupt mode */
-	MRT_MODE_ONESHOT = (1 << 1)		/*!< MRT One-shot interrupt mode */
+    MRT_MODE_REPEAT =  (0 << 1),    /*!< MRT Repeat interrupt mode */
+    MRT_MODE_ONESHOT = (1 << 1)        /*!< MRT One-shot interrupt mode */
 } MRT_MODE_T;
 
-/**
- * @brief MRT register bit fields & masks
- */
 /* MRT Time interval register bit fields */
-#define MRT_INTVAL_IVALUE        (0x7FFFFFFFUL)	/* Maximum interval load value and mask */
-#define MRT_INTVAL_LOAD          (0x80000000UL)	/* Force immediate load of timer interval register bit */
+#define MRT_INTVAL_IVALUE        (0x7FFFFFFFUL)    /* Maximum interval load value and mask */
+#define MRT_INTVAL_LOAD          (0x80000000UL)    /* Force immediate load of timer interval register bit */
 
 /* MRT Control register bit fields & masks */
 #define MRT_CTRL_INTEN_MASK      (0x01)
@@ -98,228 +89,117 @@ typedef enum MRT_MODE {
 #define MRT3_INTFLAG        (8)
 #define MRTn_INTFLAG(ch)    (1 << (ch))
 
-/**
- * @brief	Initializes the MRT
- * @return	Nothing
- */
-static inline void Chip_MRT_Init(void)
+static inline void MRT_Init(void)
 {
-	/* Enable the clock to the register interface */
-	Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_MRT);
+    /* Enable the clock to the register interface */
+    Clock_EnablePeriphClock(SYSCTL_CLOCK_MRT);
 
-	/* Reset MRT */
-	Chip_SYSCTL_PeriphReset(RESET_MRT);
+    /* Reset MRT */
+    SYSCTL_PeriphReset(RESET_MRT);
 }
 
-/**
- * @brief	De-initializes the MRT Channel
- * @return	Nothing
- */
-static inline void Chip_MRT_DeInit(void)
+static inline void MRT_DeInit(void)
 {
-	/* Disable the clock to the MRT */
-	Chip_Clock_DisablePeriphClock(SYSCTL_CLOCK_MRT);
+    /* Disable the clock to the MRT */
+    Clock_DisablePeriphClock(SYSCTL_CLOCK_MRT);
 }
 
-/**
- * @brief	Returns a pointer to the register block for a MRT channel
- * @param	ch	: MRT channel tog et register block for (0..3)
- * @return	Pointer to the MRT register block for the channel
- */
-static inline LPC_MRT_CH_T *Chip_MRT_GetRegPtr(uint8_t ch)
+static inline LPC_MRT_CH_T *MRT_GetRegPtr(uint8_t ch)
 {
-	return LPC_MRT_CH(ch);
+    return LPC_MRT_CH(ch);
 }
 
-/**
- * @brief	Returns the timer time interval value
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	Timer time interval value (IVALUE)
- */
-static inline uint32_t Chip_MRT_GetInterval(LPC_MRT_CH_T *pMRT)
+static inline uint32_t MRT_GetInterval(LPC_MRT_CH_T *pMRT)
 {
-	return pMRT->INTVAL;
+    return pMRT->INTVAL;
 }
 
-/**
- * @brief	Sets the timer time interval value
- * @param	pMRT	 : Pointer to selected MRT Channel
- * @param   interval : The interval timeout (31-bits)
- * @return	Nothing
- * @note	Setting bit 31 in timer time interval register causes the time interval value
- * to load immediately, otherwise the time interval value will be loaded in
- * next timer cycle.<br>
- * Example: Chip_MRT_SetInterval(pMRT, 0x500 | MRT_INTVAL_LOAD); // Will load timer interval immediately<br>
- * Example: Chip_MRT_SetInterval(pMRT, 0x500); // Will load timer interval after internal expires
- */
-static inline void Chip_MRT_SetInterval(LPC_MRT_CH_T *pMRT, uint32_t interval)
+static inline void MRT_SetInterval(LPC_MRT_CH_T *pMRT, uint32_t interval)
 {
-	pMRT->INTVAL = interval;
+    pMRT->INTVAL = interval;
 }
 
-/**
- * @brief	Returns the current timer value
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	The current timer value
- */
-static inline uint32_t Chip_MRT_GetTimer(LPC_MRT_CH_T *pMRT)
+static inline uint32_t MRT_GetTimer(LPC_MRT_CH_T *pMRT)
 {
-	return pMRT->TIMER;
+    return pMRT->TIMER;
 }
 
-/**
- * @brief	Returns true if the timer is enabled
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	True if enabled, Flase if not enabled
- */
-static inline bool Chip_MRT_GetEnabled(LPC_MRT_CH_T *pMRT)
+static inline bool MRT_GetEnabled(LPC_MRT_CH_T *pMRT)
 {
-	return (bool) ((pMRT->CTRL & MRT_CTRL_INTEN_MASK) != 0);
+    return (bool) ((pMRT->CTRL & MRT_CTRL_INTEN_MASK) != 0);
 }
 
-/**
- * @brief	Enables the timer
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	Nothing
- */
-static inline void Chip_MRT_SetEnabled(LPC_MRT_CH_T *pMRT)
+static inline void MRT_SetEnabled(LPC_MRT_CH_T *pMRT)
 {
-	pMRT->CTRL = MRT_CTRL_INTEN_MASK | (pMRT->CTRL & ~MRT_CTRL_RESERVED);
+    pMRT->CTRL = MRT_CTRL_INTEN_MASK | (pMRT->CTRL & ~MRT_CTRL_RESERVED);
 }
 
-/**
- * @brief	Disables the timer
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	Nothing
- */
-static inline void Chip_MRT_SetDisabled(LPC_MRT_CH_T *pMRT)
+static inline void MRT_SetDisabled(LPC_MRT_CH_T *pMRT)
 {
-	pMRT->CTRL &= ~(MRT_CTRL_INTEN_MASK | MRT_CTRL_RESERVED);
+    pMRT->CTRL &= ~(MRT_CTRL_INTEN_MASK | MRT_CTRL_RESERVED);
 }
 
-/**
- * @brief	Returns the timer mode (repeat or one-shot)
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	The current timer mode
- */
-static inline MRT_MODE_T Chip_MRT_GetMode(LPC_MRT_CH_T *pMRT)
+static inline MRT_MODE_T MRT_GetMode(LPC_MRT_CH_T *pMRT)
 {
-	return (MRT_MODE_T) (pMRT->CTRL & MRT_CTRL_MODE_MASK);
+    return (MRT_MODE_T) (pMRT->CTRL & MRT_CTRL_MODE_MASK);
 }
 
-/**
- * @brief	Sets the timer mode (repeat or one-shot)
- * @param	pMRT	: Pointer to selected MRT Channel
- * @param   mode    : Timer mode
- * @return	Nothing
- */
-static inline void Chip_MRT_SetMode(LPC_MRT_CH_T *pMRT, MRT_MODE_T mode)
+static inline void MRT_SetMode(LPC_MRT_CH_T *pMRT, MRT_MODE_T mode)
 {
-	uint32_t reg;
+    uint32_t reg;
 
-	reg = pMRT->CTRL & ~(MRT_CTRL_MODE_MASK | MRT_CTRL_RESERVED);
-	pMRT->CTRL = reg | (uint32_t) mode;
+    reg = pMRT->CTRL & ~(MRT_CTRL_MODE_MASK | MRT_CTRL_RESERVED);
+    pMRT->CTRL = reg | (uint32_t) mode;
 }
 
-/**
- * @brief	Check if the timer is configured in repeat mode
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	True if in repeat mode, False if in one-shot mode
- */
-static inline bool Chip_MRT_IsRepeatMode(LPC_MRT_CH_T *pMRT)
+static inline bool MRT_IsRepeatMode(LPC_MRT_CH_T *pMRT)
 {
-	return ((pMRT->CTRL & MRT_CTRL_MODE_MASK) != 0) ? false : true;
+    return ((pMRT->CTRL & MRT_CTRL_MODE_MASK) != 0) ? false : true;
 }
 
-/**
- * @brief	Check if the timer is configured in one-shot mode
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	True if in one-shot mode, False if in repeat mode
- */
-static inline bool Chip_MRT_IsOneShotMode(LPC_MRT_CH_T *pMRT)
+static inline bool MRT_IsOneShotMode(LPC_MRT_CH_T *pMRT)
 {
-	return ((pMRT->CTRL & MRT_CTRL_MODE_MASK) != 0) ? true : false;
+    return ((pMRT->CTRL & MRT_CTRL_MODE_MASK) != 0) ? true : false;
 }
 
-/**
- * @brief	Check if the timer has an interrupt pending
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	True if interrupt is pending, False if no interrupt is pending
- */
-static inline bool Chip_MRT_IntPending(LPC_MRT_CH_T *pMRT)
+static inline bool MRT_IntPending(LPC_MRT_CH_T *pMRT)
 {
-	return (bool) ((pMRT->STAT & MRT_STAT_INTFLAG) != 0);
+    return (bool) ((pMRT->STAT & MRT_STAT_INTFLAG) != 0);
 }
 
-/**
- * @brief	Clears the pending interrupt (if any)
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	Nothing
- */
-static inline void Chip_MRT_IntClear(LPC_MRT_CH_T *pMRT)
+static inline void MRT_IntClear(LPC_MRT_CH_T *pMRT)
 {
-	pMRT->STAT = MRT_STAT_INTFLAG | (pMRT->STAT & ~MRT_STAT_RESERVED);
+    pMRT->STAT = MRT_STAT_INTFLAG | (pMRT->STAT & ~MRT_STAT_RESERVED);
 }
 
-/**
- * @brief	Check if the timer is running
- * @param	pMRT	: Pointer to selected MRT Channel
- * @return	True if running, False if stopped
- */
-static inline bool Chip_MRT_Running(LPC_MRT_CH_T *pMRT)
+static inline bool MRT_Running(LPC_MRT_CH_T *pMRT)
 {
-	return (bool) ((pMRT->STAT & MRT_STAT_RUNNING) != 0);
+    return (bool) ((pMRT->STAT & MRT_STAT_RUNNING) != 0);
 }
 
-/**
- * @brief	Returns the IDLE channel value
- * @return	IDLE channel value (unshifted in bits 7..4)
- */
-static inline uint8_t Chip_MRT_GetIdleChannel(void)
+static inline uint8_t MRT_GetIdleChannel(void)
 {
-	return (uint8_t) (LPC_MRT->IDLE_CH);
+    return (uint8_t) (LPC_MRT->IDLE_CH);
 }
 
-/**
- * @brief	Returns the IDLE channel value
- * @return	IDLE channel value (shifted in bits 3..0)
- */
-static inline uint8_t Chip_MRT_GetIdleChannelShifted(void)
+static inline uint8_t MRT_GetIdleChannelShifted(void)
 {
-	return (uint8_t) (Chip_MRT_GetIdleChannel() >> 4);
+    return (uint8_t) (MRT_GetIdleChannel() >> 4);
 }
 
-/**
- * @brief	Returns the interrupt pending status for all MRT channels
- * @return	IRQ pending channel bitfield(bit 0 = MRT0, bit 1 = MRT1, etc.)
- */
-static inline uint32_t Chip_MRT_GetIntPending(void)
+static inline uint32_t MRT_GetIntPending(void)
 {
-	return LPC_MRT->IRQ_FLAG;
+    return LPC_MRT->IRQ_FLAG;
 }
 
-/**
- * @brief	Returns the interrupt pending status for a singel MRT channel
- * @param	ch	: Channel to check pending interrupt status for
- * @return	IRQ pending channel number
- */
-static inline bool Chip_MRT_GetIntPendingByChannel(uint8_t ch)
+static inline bool MRT_GetIntPendingByChannel(uint8_t ch)
 {
-	return (bool) (((LPC_MRT->IRQ_FLAG >> ch) & 1) != 0);
+    return (bool) (((LPC_MRT->IRQ_FLAG >> ch) & 1) != 0);
 }
 
-/**
- * @brief	Clears the interrupt pending status for one or more MRT channels
- * @param	mask	: Channels to clear (bit 0 = MRT0, bit 1 = MRT1, etc.)
- * @return	Nothing
- * @note	Use this function to clear multiple interrupt pending states in
- * a single call via the IRQ_FLAG register. Performs the same function for
- * all MRT channels in a single call as the Chip_MRT_IntClear() does for a
- * single channel.
- */
-static inline void Chip_MRT_ClearIntPending(uint32_t mask)
+static inline void MRT_ClearIntPending(uint32_t mask)
 {
-	LPC_MRT->IRQ_FLAG = mask;
+    LPC_MRT->IRQ_FLAG = mask;
 }
 
 #endif
