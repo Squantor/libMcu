b80a9f6e1ce7d4897c24afd2524ecf74a07060bf
Bart Bilos <squantmuts@gmail.com>
2019-10-04 19:41:49 +0200

    updated wakeup timer functions to camelcase

--- inc/nxp/LPC8XX/LPC8XX_wkt.h
+++ inc/nxp/LPC8XX/LPC8XX_wkt.h
@@ -53,56 +53,52 @@ typedef enum  {
     WKT_CLKSRC_10KHZ = 1    /* Low power clock - runs at 10kHz */
 } WKT_CLKSRC_T;
 
-static inline WKT_CLKSRC_T WKT_GetClockSource(LPC_WKT_T *pWKT)
+static inline WKT_CLKSRC_T WktGetClockSource(LPC_WKT_T *pWKT)
 {
     return (WKT_CLKSRC_T) (pWKT->CTRL & WKT_CTRL_CLKSEL);
 }
 
-static inline void WKT_SetClockSource(LPC_WKT_T *pWKT, WKT_CLKSRC_T clkSrc)
+static inline void WktSetClockSource(LPC_WKT_T *pWKT, WKT_CLKSRC_T clkSrc)
 {
-        if (clkSrc == WKT_CLKSRC_10KHZ) {
+    if (clkSrc == WKT_CLKSRC_10KHZ)
         pWKT->CTRL |= WKT_CTRL_CLKSEL;    /* using Low Power clock 10kHz */
-    }
-    else {
+    else
         pWKT->CTRL &= ~WKT_CTRL_CLKSEL;    /* using Divided IRC clock 750kHz */
-    }
 }
 
-static inline uint32_t WKT_GetClockRate(LPC_WKT_T *pWKT)
+static inline uint32_t WktGetClockRate(LPC_WKT_T *pWKT)
 {
-    if (WKT_GetClockSource(pWKT) == WKT_CLKSRC_DIVIRC) {
+    if (WktGetClockSource(pWKT) == WKT_CLKSRC_DIVIRC)
         /* Approximately 750KHz */
         return (750 * 1000);
-    }
-    else {
+    else
         /* Approximately 10KHz */
         return (10 * 1000);
-    }    
 }
 
-static inline bool WKT_GetIntStatus(LPC_WKT_T *pWKT)
+static inline bool WktGetIntStatus(LPC_WKT_T *pWKT)
 {
     return (bool) ((pWKT->CTRL & WKT_CTRL_ALARMFLAG) != 0);
 }
 
-static inline void WKT_ClearIntStatus(LPC_WKT_T *pWKT)
+static inline void WktClearIntStatus(LPC_WKT_T *pWKT)
 {
     pWKT->CTRL = WKT_CTRL_ALARMFLAG | (pWKT->CTRL & ~WKT_CTRL_RESERVED);
 }
 
-static inline void WKT_Stop(LPC_WKT_T *pWKT)
+static inline void WktStop(LPC_WKT_T *pWKT)
 {
     pWKT->CTRL = WKT_CTRL_CLEARCTR | (pWKT->CTRL & ~WKT_CTRL_RESERVED);
 }
 
-static inline void WKT_LoadCount(LPC_WKT_T *pWKT, uint32_t count)
+static inline void WktLoadCount(LPC_WKT_T *pWKT, uint32_t count)
 {
     pWKT->COUNT = count;
 }
 
-static inline void WKT_Start(LPC_WKT_T *pWKT, WKT_CLKSRC_T clkSrc, uint32_t cntVal)
+static inline void WktStart(LPC_WKT_T *pWKT, WKT_CLKSRC_T clkSrc, uint32_t cntVal)
 {
-    WKT_SetClockSource(pWKT, (WKT_CLKSRC_T) clkSrc);
+    WktSetClockSource(pWKT, (WKT_CLKSRC_T) clkSrc);
     pWKT->COUNT = cntVal;    
 }
 
