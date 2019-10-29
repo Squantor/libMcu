e53530d78a1c672b9b5704932b25255b971d653a
Bart Bilos <squantmuts@gmail.com>
2019-10-29 21:58:31 +0100

    cleaned up LPC81X syscon

--- inc/nxp/LPC8XX/LPC81X_syscon.h
+++ inc/nxp/LPC8XX/LPC81X_syscon.h
@@ -91,23 +91,20 @@ typedef struct {
 #define PDWAKEUPWRMASK  (0x00006D10)
 #define PDWAKEUPDATMASK (0x000080EF)
 
-/**
- * Peripheral reset identifiers
- */
 typedef enum {
-    RESET_SPI0,            /*!< SPI0 reset control */
-    RESET_SPI1,            /*!< SPI1 reset control */
-    RESET_UARTFBRG,        /*!< UART fractional baud rate generator reset control */
-    RESET_USART0,        /*!< USART0 reset control */
-    RESET_USART1,        /*!< USART1 reset control */
-    RESET_USART2,        /*!< USART2 reset control */
-    RESET_I2C0,            /*!< I2C0 reset control */
-    RESET_MRT,            /*!< MRT reset control */
-    RESET_SCT,            /*!< SCT reset control */
-    RESET_WKT,            /*!< Self wake-up timer (WKT) control */
-    RESET_GPIO,            /*!< GPIO reset control */
-    RESET_FLASH,        /*!< FLASH reset control */
-    RESET_ACMP,            /*!< ACMP reset control */
+    RESET_SPI0,
+    RESET_SPI1,
+    RESET_UARTFBRG,
+    RESET_USART0,
+    RESET_USART1,
+    RESET_USART2,
+    RESET_I2C0,
+    RESET_MRT,
+    RESET_SCT,
+    RESET_WKT,
+    RESET_GPIO,
+    RESET_FLASH,
+    RESET_ACMP,
 } SYSCTL_PERIPH_RESET_T;
 
 #include "nxp/LPC8XX/LPC8XX_syscon.h"
