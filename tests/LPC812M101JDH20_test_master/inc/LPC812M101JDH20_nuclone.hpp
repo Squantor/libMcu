ea189c2bbda9e3c8d83d0aa2dfb2b90f9fc1dc11
Bart Bilos <squantmuts@gmail.com>
2019-10-31 21:56:59 +0100

    updated debugging scripts to proper binary outputs

--- tests/LPC812M101JDH20_test_master/inc/LPC812M101JDH20_nuclone.hpp
+++ tests/LPC812M101JDH20_test_master/inc/LPC812M101JDH20_nuclone.hpp
@@ -26,57 +26,25 @@ SOFTWARE.
 
 #include <mcu_ll.h>
 
-#define GPIO_00_SWM     IOCON_PIO7
-#define GPIO_00_PIO     7
-#define GPIO_01_SWM     IOCON_PIO14
-#define GPIO_01_PIO     14
-#define GPIO_02_SWM     IOCON_PIO15
-#define GPIO_02_PIO     15
-#define GPIO_03_SWM     IOCON_PIO16
-#define GPIO_03_PIO     16
-#define GPIO_04_SWM     IOCON_PIO5
-#define GPIO_04_PIO     5
-#define GPIO_05_SWM     IOCON_PIO8
-#define GPIO_05_PIO     8
-#define GPIO_06_SWM     IOCON_PIO9
-#define GPIO_06_PIO     9
-#define GPIO_07_SWM
-#define GPIO_07_PIO
-#define GPIO_08_SWM
-#define GPIO_08_PIO
-#define GPIO_09_SWM
-#define GPIO_09_PIO
-#define GPIO_10_SWM
-#define GPIO_10_PIO
-#define GPIO_11_SWM
-#define GPIO_11_PIO
-#define GPIO_12_SWM
-#define GPIO_12_PIO
-#define GPIO_13_SWM
-#define GPIO_13_PIO
+// outside uart communications
+#define UART_OUT_TXD    (13)
+#define UART_OUT_RXD    (12)
 
-// busses
-#define I2C_SDA_SWM     IOCON_PIO10
-#define I2C_SDA_PIO     10
-#define I2C_SCL_SWM     IOCON_PIO11
-#define I2C_SCL_PIO     11
+// synchronisation pins
+#define TEST_SYNC_OUT   (17)
+#define TEST_SYNC_IN    (14)
 
-#define SPI_MOSI_SWM    IOCON_PIO17
-#define SPI_MOSI_PIO    17
-#define SPI_MISO_SWM    IOCON_PIO13
-#define SPI_MISO_PIO    13
-#define SPI_SCK_SWM     IOCON_PIO12
-#define SPI_SCK_PIO     12
-#define SPI_CE_SWM      IOCON_PIO4
-#define SPI_CE_PIO      4
+// pins used for testing
+#define TEST_UART_TXD   (16)
+#define TEST_UART_RXD   (15)
 
-// analog pins
-#define VREF_SWM        IOCON_PIO6
-#define VREF_PIO        6
-#define AN_00_SWM       IOCON_PIO0
-#define AN_00_PIO       0
-#define AN_01_SWM       IOCON_PIO1
-#define AN_01_PIO       1
+#define TEST_I2C_SCL    (10)
+#define TEST_I2C_SDA    (11)
+
+#define TEST_SPI_MOSI   (16)
+#define TEST_SPI_MISO   (15)
+
+
 
 
 // how many ticks per second
