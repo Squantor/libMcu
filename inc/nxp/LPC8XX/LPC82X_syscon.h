49fa5b5ba1ead294455e7de9c5ca90d5b8a8720c
Squantor <squantmuts@gmail.com>
2021-06-22 23:26:26 +0200

    moving old definitions

--- inc/nxp/LPC8XX/LPC82X_syscon.h
+++ inc/nxp/LPC8XX/LPC82X_syscon.h
@@ -30,93 +30,56 @@ LPC820 series common system control registers, defines and functions.
 #ifndef LPC82X_SYSCON_H
 #define LPC82X_SYSCON_H
 
-typedef struct {
-    __IO uint32_t SYSMEMREMAP;      /* System memory remap (R/W) */
-    __IO uint32_t PRESETCTRL;       /* Peripheral reset control (R/W) */
-    __IO uint32_t SYSPLLCTRL;       /* System PLL control (R/W) */
-    __IO uint32_t SYSPLLSTAT;       /* System PLL status (R/W ) */
+typedef volatile struct {
+    uint32_t SYSMEMREMAP;      /* System memory remap (R/W) */
+    uint32_t PRESETCTRL;       /* Peripheral reset control (R/W) */
+    uint32_t SYSPLLCTRL;       /* System PLL control (R/W) */
+    uint32_t SYSPLLSTAT;       /* System PLL status (R/W ) */
     uint32_t RESERVED0[4];
-    __IO uint32_t SYSOSCCTRL;       /* System oscillator control (R/W) */
-    __IO uint32_t WDTOSCCTRL;       /* Watchdog oscillator control (R/W) */
-    __IO uint32_t IRCCTRL;          /* IRC Control Register (Available only in LPC82X) */
+    uint32_t SYSOSCCTRL;       /* System oscillator control (R/W) */
+    uint32_t WDTOSCCTRL;       /* Watchdog oscillator control (R/W) */
+    uint32_t IRCCTRL;          /* IRC Control Register (Available only in LPC82X) */
     uint32_t RESERVED1[1];
-    __IO uint32_t SYSRSTSTAT;       /* System reset status Register (R/W ) */
+    uint32_t SYSRSTSTAT;       /* System reset status Register (R/W ) */
     uint32_t RESERVED2[3];
-    __IO uint32_t SYSPLLCLKSEL;     /* System PLL clock source select (R/W) */
-    __IO uint32_t SYSPLLCLKUEN;     /* System PLL clock source update enable (R/W) */
+    uint32_t SYSPLLCLKSEL;     /* System PLL clock source select (R/W) */
+    uint32_t SYSPLLCLKUEN;     /* System PLL clock source update enable (R/W) */
     uint32_t RESERVED3[10];
-    __IO uint32_t MAINCLKSEL;       /* Main clock source select (R/W) */
-    __IO uint32_t MAINCLKUEN;       /* Main clock source update enable (R/W) */
-    __IO uint32_t SYSAHBCLKDIV;     /* System AHB clock divider (R/W) */
+    uint32_t MAINCLKSEL;       /* Main clock source select (R/W) */
+    uint32_t MAINCLKUEN;       /* Main clock source update enable (R/W) */
+    uint32_t SYSAHBCLKDIV;     /* System AHB clock divider (R/W) */
     uint32_t RESERVED4[1];
-    __IO uint32_t SYSAHBCLKCTRL;    /* System AHB clock control (R/W) */
+    uint32_t SYSAHBCLKCTRL;    /* System AHB clock control (R/W) */
     uint32_t RESERVED5[4];
-    __IO uint32_t UARTCLKDIV;       /* UART clock divider (R/W) */
+    uint32_t UARTCLKDIV;       /* UART clock divider (R/W) */
     uint32_t RESERVED6[18];
-    __IO uint32_t CLKOUTSEL;        /* CLKOUT clock source select (R/W) */
-    __IO uint32_t CLKOUTUEN;        /* CLKOUT clock source update enable (R/W) */
-    __IO uint32_t CLKOUTDIV;        /* CLKOUT clock divider (R/W) */
+    uint32_t CLKOUTSEL;        /* CLKOUT clock source select (R/W) */
+    uint32_t CLKOUTUEN;        /* CLKOUT clock source update enable (R/W) */
+    uint32_t CLKOUTDIV;        /* CLKOUT clock divider (R/W) */
     uint32_t RESERVED7;
-    __IO uint32_t UARTFRGDIV;       /* UART fractional divider SUB(R/W) */
-    __IO uint32_t UARTFRGMULT;      /* UART fractional divider ADD(R/W) */
+    uint32_t UARTFRGDIV;       /* UART fractional divider SUB(R/W) */
+    uint32_t UARTFRGMULT;      /* UART fractional divider ADD(R/W) */
     uint32_t RESERVED8[1];
-    __IO uint32_t EXTTRACECMD;      /* External trace buffer command register  */
-    __IO uint32_t PIOPORCAP0;       /* POR captured PIO status 0 (R/ ) */
+    uint32_t EXTTRACECMD;      /* External trace buffer command register  */
+    uint32_t PIOPORCAP0;       /* POR captured PIO status 0 (R/ ) */
     uint32_t RESERVED9[12];
-    __IO uint32_t IOCONCLKDIV[7];   /* Peripheral clock x to the IOCON block for programmable glitch filter */
-    __IO uint32_t BODCTRL;          /* BOD control (R/W) */
-    __IO uint32_t SYSTCKCAL;        /* System tick counter calibration (R/W) */
+    uint32_t IOCONCLKDIV[7];   /* Peripheral clock x to the IOCON block for programmable glitch filter */
+    uint32_t BODCTRL;          /* BOD control (R/W) */
+    uint32_t SYSTCKCAL;        /* System tick counter calibration (R/W) */
     uint32_t RESERVED10[6];
-    __IO uint32_t IRQLATENCY;       /* IRQ delay */
-    __IO uint32_t NMISRC;           /* NMI Source Control     */
-    __IO uint32_t PINTSEL[8];       /* GPIO Pin Interrupt Select register 0 */
+    uint32_t IRQLATENCY;       /* IRQ delay */
+    uint32_t NMISRC;           /* NMI Source Control     */
+    uint32_t PINTSEL[8];       /* GPIO Pin Interrupt Select register 0 */
     uint32_t RESERVED11[27];
-    __IO uint32_t STARTERP0;        /* Start logic signal enable Register 0 (R/W) */
+    uint32_t STARTERP0;        /* Start logic signal enable Register 0 (R/W) */
     uint32_t RESERVED12[3];
-    __IO uint32_t STARTERP1;        /* Start logic signal enable Register 0 (R/W) */
+    uint32_t STARTERP1;        /* Start logic signal enable Register 0 (R/W) */
     uint32_t RESERVED13[6];
-    __IO uint32_t PDSLEEPCFG;       /* Power-down states in Deep-sleep mode (R/W) */
-    __IO uint32_t PDAWAKECFG;       /* Power-down states after wake-up (R/W) */
-    __IO uint32_t PDRUNCFG;         /* Power-down configuration Register (R/W) */
+    uint32_t PDSLEEPCFG;       /* Power-down states in Deep-sleep mode (R/W) */
+    uint32_t PDAWAKECFG;       /* Power-down states after wake-up (R/W) */
+    uint32_t PDRUNCFG;         /* Power-down configuration Register (R/W) */
     uint32_t RESERVED14[111];
-    __I  uint32_t DEVICEID;   /* Device ID (R/ ) */
+    const uint32_t DEVICEID;   /* Device ID (R/ ) */
 } LPC_SYSCTL_T;
-
-#define SYSCTL_WAKEUP_I2C1INT     (1 << 7)
-#define SYSCTL_WAKEUP_I2C2INT     (1 << 21)
-#define SYSCTL_WAKEUP_I2C3INT     (1 << 22)
-
-#define SYSCTL_SLPWAKE_ADC_PD    (1 << 4)
-
-/* Reserved bits masks for registers */
-#define SYSCTL_PRESETCTRL_RESERVED      0xfffe2000
-#define SYSCTL_SYSAHBCLKCTRL_RESERVED   0xda100000
-#define SYSCTL_PIOPORCAP0_RESERVED      0xfffc0000
-#define SYSCTL_STARTERP1_RESERVED       ((1<<2)|(1<<6)|(7<<9)|(1<<14)|0xff9f0000)
-
-/* PDWAKECFG and PDRUNCFG register masks */
-#define PDWAKEUPWRMASK  (0x00006D00)
-#define PDWAKEUPDATMASK (0x000080FF)
-
-typedef enum {
-    RESET_SPI0,
-    RESET_SPI1,
-    RESET_UARTFBRG,
-    RESET_USART0,
-    RESET_USART1,
-    RESET_USART2,
-    RESET_I2C0,
-    RESET_MRT,
-    RESET_SCT,
-    RESET_WKT,
-    RESET_GPIO,
-    RESET_FLASH,
-    RESET_ACMP,
-    RESET_I2C1 = 14,
-    RESET_I2C2,
-    RESET_I2C3,
-} SYSCTL_PERIPH_RESET_T;
-
-#include "nxp/LPC8XX/LPC8XX_syscon.h"
 
 #endif
