fee24251209ab3201292c471b7595162df1e6e72
Bart Bilos <squantmuts@gmail.com>
2021-08-01 15:51:24 +0200

    added setting clock divider ratio

--- inc/nxp/LPC8XX/LPC84X_i2c.h
+++ inc/nxp/LPC8XX/LPC84X_i2c.h
@@ -12,24 +12,36 @@
 
 /** I2C - Register Layout Typedef */
 typedef volatile struct {
-    uint32_t CFG;               /**< Configuration for shared functions., offset: 0x0 */
-    uint32_t STAT;              /**< Status register for Master, Slave, and Monitor functions., offset: 0x4 */
-    uint32_t INTENSET;          /**< Interrupt Enable Set and read register., offset: 0x8 */
-    const uint32_t INTENCLR;    /**< Interrupt Enable Clear register., offset: 0xC */
-    uint32_t TIMEOUT;           /**< Time-out value register., offset: 0x10 */
-    uint32_t CLKDIV;            /**< Clock pre-divider for the entire I2C interface. This determines what time increments are used for the MSTTIME register, and controls some timing of the Slave function., offset: 0x14 */
-    const uint32_t INTSTAT;     /**< Interrupt Status register for Master, Slave, and Monitor functions., offset: 0x18 */
+    uint32_t CFG;               /*!< Configuration for shared functions., offset: 0x0 */
+    uint32_t STAT;              /*!< Status register for Master, Slave, and Monitor functions., offset: 0x4 */
+    uint32_t INTENSET;          /*!< Interrupt Enable Set and read register., offset: 0x8 */
+    const uint32_t INTENCLR;    /*!< Interrupt Enable Clear register., offset: 0xC */
+    uint32_t TIMEOUT;           /*!< Time-out value register., offset: 0x10 */
+    uint32_t CLKDIV;            /*!< Clock pre-divider for the entire I2C interface. This determines what time increments are used for the MSTTIME register, and controls some timing of the Slave function., offset: 0x14 */
+    const uint32_t INTSTAT;     /*!< Interrupt Status register for Master, Slave, and Monitor functions., offset: 0x18 */
     uint8_t RESERVED_0[4];
-    uint32_t MSTCTL;            /**< Master control register., offset: 0x20 */
-    uint32_t MSTTIME;           /**< Master timing configuration., offset: 0x24 */
-    uint32_t MSTDAT;            /**< Combined Master receiver and transmitter data register., offset: 0x28 */
+    uint32_t MSTCTL;            /*!< Master control register., offset: 0x20 */
+    uint32_t MSTTIME;           /*!< Master timing configuration., offset: 0x24 */
+    uint32_t MSTDAT;            /*!< Combined Master receiver and transmitter data register., offset: 0x28 */
     uint8_t RESERVED_1[20];
-    uint32_t SLVCTL;            /**< Slave control register., offset: 0x40 */
-    uint32_t SLVDAT;            /**< Combined Slave receiver and transmitter data register., offset: 0x44 */
-    uint32_t SLVADR[4];         /**< Slave address register., array offset: 0x48, array step: 0x4 */
-    uint32_t SLVQUAL0;          /**< Slave Qualification for address 0., offset: 0x58 */
+    uint32_t SLVCTL;            /*!< Slave control register., offset: 0x40 */
+    uint32_t SLVDAT;            /*!< Combined Slave receiver and transmitter data register., offset: 0x44 */
+    uint32_t SLVADR[4];         /*!< Slave address register., array offset: 0x48, array step: 0x4 */
+    uint32_t SLVQUAL0;          /*!< Slave Qualification for address 0., offset: 0x58 */
     uint8_t RESERVED_2[36];
-    const uint32_t MONRXDAT;    /**< Monitor receiver data register., offset: 0x80 */
+    const uint32_t MONRXDAT;    /*!< Monitor receiver data register., offset: 0x80 */
 } I2C_Type;
+
+#define I2C_CLKDIV_RESERVED    (0xFFFF0000)    /*!< I2C clock divide register reserved bits */
+
+/**
+ * @brief   Set clock division ratio
+ * @param   peripheral  base address of I2C peripheral
+ * @param   divider     divider value, 0 is passthrough
+ */
+static inline void i2cSetClockDivider(I2C_Type *peripheral, uint32_t divider)
+{
+    peripheral->CLKDIV = divider & ~I2C_CLKDIV_RESERVED;
+} 
 
 #endif
