fd4f882a10e9fdc82491698562074c8323f1c267
Squantor <squantmuts@gmail.com>
2021-11-10 23:09:48 +0100

    WIP input mux functions

--- inc/nxp/LPC8XX/LPC84X_inputmux.h
+++ inc/nxp/LPC8XX/LPC84X_inputmux.h
@@ -12,11 +12,44 @@
 
 /** INPUTMUX - Register Layout Typedef */
 typedef volatile struct INPUTMUX_Struct {
-    uint32_t DMA_INMUX_INMUX[2];    /**< DMA output trigger selection to become DMA trigger, array offset: 0x0, array step: 0x4 */
+    uint32_t DMA_INMUX_INMUX[2];    /*!< DMA output trigger selection to become DMA trigger, array offset: 0x0, array step: 0x4 */
     uint8_t RESERVED_0[24];
-    uint32_t SCT_INMUX[4];          /**< input select register for SCT, array offset: 0x20, array step: 0x4 */
+    uint32_t SCT_INMUX[4];          /*!< input select register for SCT, array offset: 0x20, array step: 0x4 */
     uint8_t RESERVED_1[16];
-    uint32_t DMA_ITRIG_INMUX[25];   /**< Trigger select register for DMA channel, array offset: 0x40, array step: 0x4 */
+    uint32_t DMA_ITRIG_INMUX[25];   /*!< Trigger select register for DMA channel, array offset: 0x40, array step: 0x4 */
 } INPUTMUX_Type;
+
+/**
+ * SCT Input values enum, TODO: move to device specifics
+ */
+typedef enum SCT_INMUX_INPUT {
+    SCT_INMUX_INPUT_0 = 0,    /*!< SCT Input 0 */
+    SCT_INMUX_INPUT_1 = 1,    /*!< SCT Input 1 */
+    SCT_INMUX_INPUT_2 = 2,    /*!< SCT Input 2 */
+    SCT_INMUX_INPUT_3 = 3,    /*!< SCT Input 3 */
+} SCT_INMUX_INPUT_Type;
+
+/**
+ * SCT Inputs muxing options
+ */
+typedef enum SCT_INPUT_MUX {
+    SCT_MUX_PIN0 = 0,           /*!< SCT input pin 0 */
+    SCT_MUX_PIN1 = 1,           /*!< SCT input pin 1 */
+    SCT_MUX_PIN2 = 2,           /*!< SCT input pin 2 */
+    SCT_MUX_PIN3 = 3,           /*!< SCT input pin 3 */
+    SCT_MUX_ADC_THCMP_IRQ = 4,  /*!< ADC threshold comparator IRQ */
+    SCT_MUX_ACMP_O = 5,         /*!< TODO */
+    SCT_MUX_T0_MAT2 = 6,        /*!< TODO */
+    SCT_MUX_GPIOINT_BMATCH = 7, /*!< TODO */
+    SCT_MUX_ARM_TXEV = 8,       /*!< TODO */
+    SCT_MUX_DEBUG_HALTED = 9,   /*!< TODO */
+} SCT_INPUT_MUX_Type;
+
+#define SCT_INMUX_RESERVED  (0xFFFFFFF0)
+
+static inline void inputMuxSct(INPUTMUX_Type *peripheral, SCT_INMUX_INPUT_Type input, SCT_INPUT_MUX_Type setting)
+{
+    peripheral->SCT_INMUX[input] = setting & ~SCT_INMUX_RESERVED;
+}
 
 #endif
