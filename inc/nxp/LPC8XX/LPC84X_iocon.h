6509e0c7d2f329f1aa16c6633ae4707c5d5b65f5
Squantor <squantmuts@gmail.com>
2021-12-21 21:41:11 +0100

    harmonizing iocon

--- inc/nxp/LPC8XX/LPC84X_iocon.h
+++ inc/nxp/LPC8XX/LPC84X_iocon.h
@@ -4,69 +4,27 @@
  * Copyright (c) 2021 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
  */
-/* 
+/*
  * LPC84X series I/O Control registers, defines and functions.
  */
 #ifndef LPC84X_IOCON_H
 #define LPC84X_IOCON_H
 
 typedef volatile struct IOCON_Struct {
-    uint32_t PIO[56];    /**< Digital I/O control for pins PIO0_17..Digital I/O control for pins PIO1_10, array offset: 0x0, array step: 0x4 */
+  uint32_t
+      PIO[56]; /**< Digital I/O control for pins PIO0_17..Digital I/O control
+                  for pins PIO1_10, array offset: 0x0, array step: 0x4 */
 } IOCON_Type;
 
-#define IOCON_RESERVED_PRESET   (0x00000080u)   /**< Default setting for IOCON register, some reserved bits are set. */
-#define IOCON_RESERVED_MASK     (0x0001FF78u)   /**< Masking off reserved pins so they do not accidentaly get set */
+#define IOCON_RESERVED_PRESET                                               \
+  (0x00000080u) /**< Default setting for IOCON register, some reserved bits \
+                   are set. */
+#define IOCON_RESERVED_MASK                                                   \
+  (0x0001FF78u) /**< Masking off reserved pins so they do not accidentaly get \
+                   set */
 
-#define IOCON_MODE_INACTIVE (0 << 3)    /**< Pin inactive, no pullup/down resistor enabled */
-#define IOCON_MODE_PULLDOWN (1 << 3)    /**< Pulldown resistor enabled */
-#define IOCON_MODE_PULLUP   (2 << 3)    /**< Pullup resistor enabled */
-#define IOCON_MODE_REPEATER (3 << 3)    /**< Repeater mode */
-#define IOCON_HYST_EN       (1 << 5)    /**< Enable hysterhesis */
-#define IOCON_INVERT_IN     (1 << 6)    /**< Invert input enable */
+#define IOCON_DAC_ENABLE (1 << 16)
 
-typedef enum {
-    IOCON_I2CMODE_STD = 0,  /**< Standard mode/Fast mode I2C */
-    IOCON_I2CMODE_GPIO = 1,  /**< GPIO mode */
-    IOCON_I2CMODE_FAST = 2,  /**< Fast-mode Plus I2C */
-} IOCON_I2CMODE_Type;
-
-#define IOCON_I2CMODE   ((x & 0x03) << 8)   /**< dedicated I2C pin mode */
-#define IOCON_OD        (1 << 10)           /**< Open drain mode */
-
-/* Iocon input filter settings */
-typedef enum {
-    IOCON_S_MODE_BYPASS = 0,    /**< Bypass input filter */
-    IOCON_S_MODE_1CLOCK = 1,    /**< Pulses shorter then 1 clock are rejected */
-    IOCON_S_MODE_2CLOCK = 2,    /**< Pulses shorter then 2 clocks are rejected */
-    IOCON_S_MODE_3CLOCK = 3,    /**< Pulses shorter then 3 clocks are rejected */
-} IOCON_S_MODE_Type;
-
-#define IOCON_S_MODE(x) ((x & 0x03) << 11)
-
-/* Iocon clock select for input filter */
-typedef enum {
-    IOCONCLKDIV0 = 0,   /**< select IOCONCLKDIV0 */
-    IOCONCLKDIV1 = 1,   /**< select IOCONCLKDIV1 */
-    IOCONCLKDIV2 = 2,   /**< select IOCONCLKDIV2 */
-    IOCONCLKDIV3 = 3,   /**< select IOCONCLKDIV3 */
-    IOCONCLKDIV4 = 4,   /**< select IOCONCLKDIV4 */
-    IOCONCLKDIV5 = 5,   /**< select IOCONCLKDIV5 */
-    IOCONCLKDIV6 = 6,   /**< select IOCONCLKDIV6 */
-} IOCON_CLK_DIV_Type;
-
-#define IOCON_CLK_DIV(x) ((x & 0x03) << 13)
-#define IOCON_DAC_ENABLE    (1 << 16)
-
-/**
- * @brief   Setup IOCON pin setting in one go
- * @param   peripheral  : base address of IOCON peripheral
- * @param   pin         : IOCON pin index
- * @param   settings    : Settings to apply to IOCON pin
- * @return  Nothing
- */
-static inline void ioconSetupPin(IOCON_Type *peripheral, const IOCON_PIN_Type pin, const uint32_t settings)
-{
-    peripheral->PIO[pin] = (settings & IOCON_RESERVED_MASK) | IOCON_RESERVED_PRESET;
-}
+#include "nxp/LPC8XX/LPC8XX_iocon.h"
 
 #endif
--- inc/nxp/LPC8XX/LPC8XX_iocon.h
+++ inc/nxp/LPC8XX/LPC8XX_iocon.h
@@ -4,45 +4,67 @@ SPDX-License-Identifier: Unlicense
 Copyright (c) 2021 Bart Bilos
 For conditions of distribution and use, see LICENSE file
 */
-/* 
+/*
 LPC800 series common IOCON registers, defines and functions.
 */
 #ifndef LPC8XX_IOCON_H
 #define LPC8XX_IOCON_H
 
-typedef volatile struct {
-    uint32_t PIO[31];
-} IOCON_Type;
+typedef volatile struct { uint32_t PIO[31]; } IOCON_Type;
 
-#define IOCON_RESERVED_PRESET   (0x00000080u)   /**< Default setting for IOCON register, some reserved bits are set. */
-#define IOCON_RESERVED_MASK     (0x0000FF78u)   /**< Masking off reserved pins so they do not accidentaly get set */
-
-#define IOCON_MODE_INACTIVE (0 << 3)    /**< Pin inactive, no pullup/down resistor enabled */
-#define IOCON_MODE_PULLDOWN (1 << 3)    /**< Pulldown resistor enabled */
-#define IOCON_MODE_PULLUP   (2 << 3)    /**< Pullup resistor enabled */
-#define IOCON_MODE_REPEATER (3 << 3)    /**< Repeater mode */
-#define IOCON_HYST_EN       (1 << 5)    /**< Enable hysterhesis */
-#define IOCON_INVERT_IN     (1 << 6)    /**< Invert input enable */
+#define IOCON_RESERVED_PRESET                                                 \
+  (0x00000080u) /**< Reserved settings for IOCON register, some reserved bits \
+                   are set. */
+#define IOCON_RESERVED_MASK                                                   \
+  (0x0000FF78u) /**< Masking off reserved pins so they do not accidentaly get \
+                   set */
 
 typedef enum {
-    IOCON_I2CMODE_STD = 0,  /**< Standard mode/Fast mode I2C */
-    IOCON_I2CMODE_GPIO = 1,  /**< GPIO mode */
-    IOCON_I2CMODE_FAST = 2,  /**< Fast-mode Plus I2C */
+  PIN_INACTIVE = 0, /**< Pin inactive, no pullup/down resistor enabled */
+  PULLDOWN = 1,     /**< Pulldown resistor enabled */
+  PULLUP = 2,       /**< Pullup resistor enabled */
+  REPEATER = 3,     /**< Repeater mode */
+} IOCON_MODE_Type;
+#define IOCON_MODE(value) ((value & 3) << 2) /**< function mode select*/
+
+#define IOCON_HYST(value) ((value & 1) << 5)   /**< Enable hysterhesis */
+#define IOCON_INVERT(value) ((value & 1) << 6) /**< Invert input enable */
+
+typedef enum {
+  IOCON_I2CMODE_STD = 0,  /**< Standard mode/Fast mode I2C */
+  IOCON_I2CMODE_GPIO = 1, /**< GPIO mode */
+  IOCON_I2CMODE_FAST = 2, /**< Fast-mode Plus I2C */
 } IOCON_I2CMODE_Type;
 
-#define IOCON_I2CMODE   ((x & 0x03) << 8)   /**< dedicated I2C pin mode */
-#define IOCON_OD        (1 << 10)           /**< Open drain mode */
+#define IOCON_I2CMODE(value) \
+  ((value & 0x03) << 8) /**< dedicated I2C pin mode */
+
+#define IOCON_OD(value) (value << 10) /**< Open drain mode */
 
 /* Iocon input filter settings */
 typedef enum {
-    IOCON_S_MODE_BYPASS = 0,    /**< Bypass input filter */
-    IOCON_S_MODE_1CLOCK = 1,    /**< Pulses shorter then 1 clock are rejected */
-    IOCON_S_MODE_2CLOCK = 2,    /**< Pulses shorter then 2 clocks are rejected */
-    IOCON_S_MODE_3CLOCK = 3,    /**< Pulses shorter then 3 clocks are rejected */
+  IOCON_S_MODE_BYPASS = 0, /**< Bypass input filter */
+  IOCON_S_MODE_1CLOCK = 1, /**< Pulses shorter then 1 clock are rejected */
+  IOCON_S_MODE_2CLOCK = 2, /**< Pulses shorter then 2 clocks are rejected */
+  IOCON_S_MODE_3CLOCK = 3, /**< Pulses shorter then 3 clocks are rejected */
 } IOCON_S_MODE_Type;
 
-#define IOCON_CLK_DIV(x) ((x & 0x03) << 13)
-#define IOCON_DAC_ENABLE    (1 << 16)
+#define IOCON_S_MODE(value) \
+  ((value & 0x03) << 11) /**< Digital filter sample mode */
+
+/* Select peripheral clock divider for input filter sampling clock. */
+typedef enum {
+  IOCON_CLK_DIV_0 = 0, /**< Clock division of zero */
+  IOCON_CLK_DIV_1 = 1, /**< Clock division of zero */
+  IOCON_CLK_DIV_2 = 2, /**< Clock division of zero */
+  IOCON_CLK_DIV_3 = 3, /**< Clock division of zero */
+  IOCON_CLK_DIV_4 = 4, /**< Clock division of zero */
+  IOCON_CLK_DIV_5 = 5, /**< Clock division of zero */
+  IOCON_CLK_DIV_6 = 6, /**< Clock division of zero */
+} IOCON_CLK_DIV_Type;
+
+#define IOCON_CLK_DIV(value) \
+  ((value & 0x03) << 13) /**< Clock divider select for input filter */
 
 /**
  * @brief   Setup IOCON pin setting in one go
@@ -51,11 +73,11 @@ typedef enum {
  * @param   settings    : Settings to apply to IOCON pin
  * @return  Nothing
  */
-static inline void ioconSetupPin(IOCON_Type *peripheral, const IOCON_PIN_Type pin, const uint32_t settings)
-{
-    peripheral->PIO[pin] = (settings & IOCON_RESERVED_MASK) | IOCON_RESERVED_PRESET;
+static inline void ioconSetupPin(IOCON_Type *peripheral,
+                                 const IOCON_PIN_Type pin,
+                                 const uint32_t settings) {
+  peripheral->PIO[pin] =
+      (settings & IOCON_RESERVED_MASK) | IOCON_RESERVED_PRESET;
 }
-
-#include "nxp/LPC8XX/LPC8XX_iocon_old.h"
 
 #endif
