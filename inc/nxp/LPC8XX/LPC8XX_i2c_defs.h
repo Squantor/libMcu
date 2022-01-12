9a1f033083fe265e9eefe0c2e8d713dbba3ad962
Squantor <squantmuts@gmail.com>
2022-01-12 13:08:08 +0100

    refactoring i2c handling for LPC8XX

--- inc/nxp/LPC8XX/LPC8XX_i2c_defs.h
+++ inc/nxp/LPC8XX/LPC8XX_i2c_defs.h
@@ -30,180 +30,95 @@ LPC800 series common I2C bus registers, defines and functions.
 #ifndef LPC8XX_I2C_DEFS_H
 #define LPC8XX_I2C_DEFS_H
 
-/* I2C register block structure */
+/** I2C - Register Layout Typedef */
 typedef volatile struct {      /*!< I2C0 Structure */
-  uint32_t CFG;                /*!< I2C Configuration Register common for Master, Slave and Monitor */
-  uint32_t STAT;               /*!< I2C Status Register common for Master, Slave and Monitor */
-  uint32_t INTENSET;           /*!< I2C Interrupt Enable Set Register common for Master, Slave and Monitor */
-  uint32_t INTENCLR;           /*!< I2C Interrupt Enable Clear Register common for Master, Slave and Monitor */
-  uint32_t TIMEOUT;            /*!< I2C Timeout value Register */
-  uint32_t CLKDIV;             /*!< I2C Clock Divider Register */
-  const uint32_t INTSTAT;      /*!< I2C Interrupt Status Register */
+  uint32_t CFG;                /*!< Configuration for shared functions */
+  uint32_t STAT;               /*!< Status register for Master, Slave, and Monitor functions */
+  uint32_t INTENSET;           /*!< Interrupt Enable Set and read register */
+  uint32_t INTENCLR;           /*!< Interrupt Enable Clear register */
+  uint32_t TIMEOUT;            /*!< Time-out value register */
+  uint32_t CLKDIV;             /*!< Clock pre-divider for the entire I2C interface */
+  const uint32_t INTSTAT;      /*!< Interrupt Status register for Master, Slave, and Monitor functions */
   const uint32_t RESERVED0;    /*!< reserved */
-  uint32_t MSTCTL;             /*!< I2C Master Control Register */
-  uint32_t MSTTIME;            /*!< I2C Master Time Register for SCL */
-  uint32_t MSTDAT;             /*!< I2C Master Data Register */
+  uint32_t MSTCTL;             /*!< Master control register */
+  uint32_t MSTTIME;            /*!< Master timing configuration */
+  uint32_t MSTDAT;             /*!< Combined Master receiver and transmitter data register */
   const uint32_t RESERVED1[5]; /*!< reserved */
-  uint32_t SLVCTL;             /*!< I2C Slave Control Register */
-  uint32_t SLVDAT;             /*!< I2C Slave Data Register */
-  uint32_t SLVADR[4];          /*!< I2C Slave Address Registers */
-  uint32_t SLVQUAL0;           /*!< I2C Slave Address Qualifier 0 Register */
+  uint32_t SLVCTL;             /*!< Slave control register */
+  uint32_t SLVDAT;             /*!< Combined Slave receiver and transmitter data register */
+  uint32_t SLVADR[4];          /*!< Slave address register */
+  uint32_t SLVQUAL0;           /*!< Slave Qualification for address 0 */
   const uint32_t RESERVED2[9]; /*!< reserved */
-  const uint32_t MONRXDAT;     /*!< I2C Monitor Data Register */
+  const uint32_t MONRXDAT;     /*!< Monitor receiver data register */
 } I2C_Type;
 
-/* Reserved bits masks for registers */
-#define I2C_CFG_RESERVED (~0x1f)
-#define I2C_STAT_RESERVED ((1 << 5) | (1 << 7) | (0xf << 20) | (0x3fu << 26))
-#define I2C_INTENSET_RESERVED ((7 << 1) | (1 << 5) | (1 << 7) | (3 << 9) | (7 << 12) | (1 << 18) | (0xf << 20) | (0x3fu << 26))
-#define I2C_INTENCLR_RESERVED ((7 << 1) | (1 << 5) | (1 << 7) | (3 << 9) | (7 << 12) | (1 << 18) | (0xf << 20) | (0x3fu << 26))
-#define I2C_TIMEOUT_RESERVED 0xffff0000
-#define I2C_CLKDIV_RESERVED 0xffff0000
-#define I2C_INTSTAT_RESERVED ((7 << 1) | (1 << 5) | (1 << 7) | (3 << 9) | (7 << 12) | (1 << 18) | (0xf << 20) | (0x3fu << 26))
-#define I2C_MSTCTL_RESERVED (~7)
-#define I2C_MSTTIME_RESERVED (~0x7f)
-#define I2C_MSTDAT_RESERVED (~0xff)
-#define I2C_SLVCTL_RESERVED (~3)
-#define I2C_SLVDAT_RESERVED (~0xff)
-#define I2C_SLVADR_RESERVED (~0xff)
-#define I2C_SLVQUAL0_RESERVED (~0xff)
+#define I2C_CFG_RESERVED (0xFFFFFFE0) /*!< I2C configuration register reserved bits */
+#define I2C_CFG_MSTEN (1 << 0)        /*!< I2C Master enable */
+#define I2C_CFG_SLVEN (1 << 1)        /*!< I2C Slave enable */
+#define I2C_CFG_MONEN (1 << 2)        /*!< I2C Monitor enable */
+#define I2C_CFG_TIMEOUTEN (1 << 3)    /*!< I2C Timeout enable */
+#define I2C_CFG_MONCLKSTR (1 << 4)    /*!< I2C monitor clock stretching enable */
 
-/* I2C Configuration register Bit definition */
-#define I2C_CFG_MSTEN (1 << 0)        /*!< Master Enable/Disable Bit */
-#define I2C_CFG_SLVEN (1 << 1)        /*!< Slave Enable/Disable Bit */
-#define I2C_CFG_MONEN (1 << 2)        /*!< Monitor Enable/Disable Bit */
-#define I2C_CFG_TIMEOUTEN (1 << 3)    /*!< Timeout Enable/Disable Bit */
-#define I2C_CFG_MONCLKSTR (1 << 4)    /*!< Monitor Clock Stretching Bit */
-#define I2C_CFG_MASK ((uint32_t)0x1F) /*!< Configuration register Mask */
+#define I2C_STAT_RESERVED (0xFCF000A0)                /*!< I2C status register reserved bits */
+#define I2C_STAT_MSTPENDING (1 << 0)                  /*!< I2C Master pending or idle*/
+#define I2C_STAT_MSTSTATE(status) (status & (7 << 1)) /*!< I2C Master status mask macro */
+typedef enum {
+  I2C_STAT_MSSTATE_IDLE = (0x0 << 1),           /*!< I2C master is in idle state */
+  I2C_STAT_MSSTATE_RECEIVE_READY = (0x1 << 1),  /*!< I2C master has received data available */
+  I2C_STAT_MSSTATE_TRANSMIT_READY = (0x2 << 1), /*!< I2C master can transmit data */
+  I2C_STAT_MSSTATE_NACK_ADDRESS = (0x3 << 1),   /*!< I2C master did not get address acknowledged */
+  I2C_STAT_MSSTATE_NACK_DATA = (0x4 << 1),      /*!< I2C master did not get data acknowledged */
+} I2C_STAT_MSSTATE_Type;
+#define I2C_STAT_MSTARBLOSS (1 << 4)                  /*!< Master lost arbitration */
+#define I2C_STAT_MSTSTSTPERR (1 << 6)                 /*!< Master start stop error flag */
+#define I2C_STAT_SLVPENDING (1 << 8)                  /*!< Slave pending */
+#define I2C_STAT_SLVSTATE(status) (status & (3 << 9)) /*!< I2C Slave status mask macro */
+typedef enum {
+  I2C_STAT_SLVSTATE_ADDR = (0x0 << 9),     /*!< I2C slave address matched */
+  I2C_STAT_SLVSTATE_RECEIVE = (0x1 << 9),  /*!< I2C slave has received data available */
+  I2C_STAT_SLVSTATE_TRANSMIT = (0x2 << 9), /*!< I2C slave can transmit data */
+} I2C_STAT_SLVSTATE_Type;
+#define I2C_STAT_SLVNOTSTR (1 << 11)                 /*!< Slave not clock stretching */
+#define I2C_STAT_SLVIDX(status) (status & (3 << 12)) /*!< Slave address match index */
+typedef enum {
+  I2C_STAT_SLVIDX_ADDR0 = (0x0 << 12), /*!< I2C slave address 0 matched */
+  I2C_STAT_SLVIDX_ADDR1 = (0x1 << 12), /*!< I2C slave address 1 matched */
+  I2C_STAT_SLVIDX_ADDR2 = (0x2 << 12), /*!< I2C slave address 2 matched */
+  I2C_STAT_SLVIDX_ADDR3 = (0x3 << 12), /*!< I2C slave address 3 matched */
+} I2C_STAT_SLVIDX_Type;
+#define I2C_STAT_SLVSEL (1 << 14)       /*!< Slave selected flag */
+#define I2C_STAT_SLVDESEL (1 << 15)     /*!< Slave deselected flag */
+#define I2C_STAT_MONRDY (1 << 16)       /*!< Monitor ready flag*/
+#define I2C_STAT_MONOV (1 << 17)        /*!< Monitor overflow flag */
+#define I2C_STAT_MONACTIVE (1 << 18)    /*!< Monitor bus active flag */
+#define I2C_STAT_MONIDLE (1 << 19)      /*!< Monitor bus idle flag */
+#define I2C_STAT_EVENTTIMEOUT (1 << 24) /*!< Event timeout flag */
+#define I2C_STAT_SCLTIMEOUT (1 << 25)   /*!< SCL timeout flag */
 
-/* I2C Status register Bit definition */
-#define I2C_STAT_MSTPENDING (1 << 0)    /*!< Master Pending Status Bit */
-#define I2C_STAT_MSTSTATE (0x7 << 1)    /*!< Master State Code */
-#define I2C_STAT_MSTRARBLOSS (1 << 4)   /*!< Master Arbitration Loss Bit */
-#define I2C_STAT_MSTSTSTPERR (1 << 6)   /* Master Start Stop Error Bit */
-#define I2C_STAT_SLVPENDING (1 << 8)    /* Slave Pending Status Bit */
-#define I2C_STAT_SLVSTATE (0x3 << 9)    /* Slave State Code */
-#define I2C_STAT_SLVNOTSTR (1 << 11)    /* Slave not stretching Clock Bit */
-#define I2C_STAT_SLVIDX (0x3 << 12)     /* Slave Address Index */
-#define I2C_STAT_SLVSEL (1 << 14)       /* Slave Selected Bit */
-#define I2C_STAT_SLVDESEL (1 << 15)     /* Slave Deselect Bit */
-#define I2C_STAT_MONRDY (1 << 16)       /* Monitor Ready Bit */
-#define I2C_STAT_MONOV (1 << 17)        /* Monitor Overflow Flag */
-#define I2C_STAT_MONACTIVE (1 << 18)    /* Monitor Active Flag */
-#define I2C_STAT_MONIDLE (1 << 19)      /* Monitor Idle Flag */
-#define I2C_STAT_EVENTTIMEOUT (1 << 24) /* Event Timeout Interrupt Flag */
-#define I2C_STAT_SCLTIMEOUT (1 << 25)   /* SCL Timeout Interrupt Flag */
+#define I2C_MSCTL_RESERVED (0xFFFFFFF0) /*!< I2C master data register reserved bits */
+#define I2C_MSCTL_MSTCONTINUE (1 << 0)  /*!< I2C master continue to next operation */
+#define I2C_MSCTL_MSTSTART (1 << 1)     /*!< I2C master generate start condition */
+#define I2C_MSCTL_MSTSTOP (1 << 2)      /*!< I2C master generate stop condition */
+#define I2C_MSCTL_MSTDMA (1 << 3)       /*!< I2C master DMA generation enable */
 
-#define I2C_STAT_MSTCODE_IDLE (0)    /* Master Idle State Code */
-#define I2C_STAT_MSTCODE_RXREADY (1) /* Master Receive Ready State Code */
-#define I2C_STAT_MSTCODE_TXREADY (2) /* Master Transmit Ready State Code */
-#define I2C_STAT_MSTCODE_NACKADR (3) /* Master NACK by slave on address State Code */
-#define I2C_STAT_MSTCODE_NACKDAT (4) /* Master NACK by slave on data State Code */
+#define I2C_MSDAT_RESERVED (0xFFFFFF00) /*!< I2C master data register reserved bits */
 
-#define I2C_STAT_SLVCODE_ADDR (0) /* Master Idle State Code */
-#define I2C_STAT_SLVCODE_RX (1)   /* Received data is available Code */
-#define I2C_STAT_SLVCODE_TX (2)   /* Data can be transmitted Code */
+#define I2C_CLKDIV_RESERVED (0xFFFF0000) /*!< I2C clock divide register reserved bits */
 
-/* I2C Interrupt Enable Set register Bit definition */
-#define I2C_INTENSET_MSTPENDING (1 << 0)  /* Master Pending Interrupt Enable Bit */
-#define I2C_INTENSET_MSTRARBLOSS (1 << 4) /* Master Arbitration Loss Interrupt Enable Bit */
-#define I2C_INTENSET_MSTSTSTPERR (1 << 6) /* Master Start Stop Error Interrupt Enable Bit */
-#define I2C_INTENSET_SLVPENDING (1 << 8)  /* Slave Pending Interrupt Enable Bit */
-#define I2C_INTENSET_SLVNOTSTR (1 << 11)  /* Slave not stretching Clock Interrupt Enable Bit */
-#define I2C_INTENSET_SLVDESEL (1 << 15)   /* Slave Deselect Interrupt Enable Bit */
-#define I2C_INTENSET_MONRDY (1 << 16)     /* Monitor Ready Interrupt Enable Bit */
-#define I2C_INTENSET_MONOV                                                           \
-  (1 << 17)                                 /* Monitor Overflow Interrupt Enable Bit \
-                                             */
-#define I2C_INTENSET_MONIDLE (1 << 19)      /* Monitor Idle Interrupt Enable Bit */
-#define I2C_INTENSET_EVENTTIMEOUT (1 << 24) /* Event Timeout Interrupt Enable Bit */
-#define I2C_INTENSET_SCLTIMEOUT                 \
-  (1 << 25) /* SCL Timeout Interrupt Enable Bit \
-             */
+#define I2C_SLVCTL_RESERVED (0xFFFFFFF4) /*!< I2C slave control register reserved bits */
+#define I2C_SLVCTL_SLVCONTINUE (1 << 0)  /*!< I2C slave continue operation */
+#define I2C_SLVCTL_SLVNACK (1 << 1)      /*!< I2C slave not acknowledge */
+#define I2C_SLVCTL_SLVDMA (1 << 3)       /*!< I2C slave DMA enable */
 
-/* I2C Interrupt Enable Clear register Bit definition */
-#define I2C_INTENCLR_MSTPENDING (1 << 0)  /* Master Pending Interrupt Clear Bit */
-#define I2C_INTENCLR_MSTRARBLOSS (1 << 4) /* Master Arbitration Loss Interrupt Clear Bit */
-#define I2C_INTENCLR_MSTSTSTPERR (1 << 6) /* Master Start Stop Error Interrupt Clear Bit */
-#define I2C_INTENCLR_SLVPENDING                                               \
-  (1 << 8)                               /* Slave Pending Interrupt Clear Bit \
-                                          */
-#define I2C_INTENCLR_SLVNOTSTR (1 << 11) /* Slave not stretching Clock Interrupt Clear Bit */
-#define I2C_INTENCLR_SLVDESEL                                               \
-  (1 << 15)                           /* Slave Deselect Interrupt Clear Bit \
-                                       */
-#define I2C_INTENCLR_MONRDY (1 << 16) /* Monitor Ready Interrupt Clear Bit */
-#define I2C_INTENCLR_MONOV                                                          \
-  (1 << 17)                                 /* Monitor Overflow Interrupt Clear Bit \
-                                             */
-#define I2C_INTENCLR_MONIDLE (1 << 19)      /* Monitor Idle Interrupt Clear Bit */
-#define I2C_INTENCLR_EVENTTIMEOUT (1 << 24) /* Event Timeout Interrupt Clear Bit */
-#define I2C_INTENCLR_SCLTIMEOUT                \
-  (1 << 25) /* SCL Timeout Interrupt Clear Bit \
-             */
+#define I2C_SLVADR_RESERVED (0xFFFFFF00) /*!< I2C slave address match register reserved bits */
+#define I2C_SLVADR_IGNORE (1 << 0)       /*!< I2C slave adress match ignore flag */
+typedef enum {
+  I2C_SLVADR_0 = 0, /*!< I2C slave address match index 0 */
+  I2C_SLVADR_1 = 1, /*!< I2C slave address match index 1 */
+  I2C_SLVADR_2 = 2, /*!< I2C slave address match index 2 */
+  I2C_SLVADR_3 = 3, /*!< I2C slave address match index 3 */
+} I2C_SLVADR_INDEX_Type;
 
-/* I2C TimeOut Value Macro */
-#define I2C_TIMEOUT_VAL(n)                                                    \
-  (((uint32_t)((n)-1) & 0xFFF0) | 0x000F) /* Macro for Timeout value register \
-                                           */
-
-/* I2C Interrupt Status register Bit definition */
-#define I2C_INTSTAT_MSTPENDING (1 << 0)  /* Master Pending Interrupt Status Bit */
-#define I2C_INTSTAT_MSTRARBLOSS (1 << 4) /* Master Arbitration Loss Interrupt Status Bit */
-#define I2C_INTSTAT_MSTSTSTPERR (1 << 6) /* Master Start Stop Error Interrupt Status Bit */
-#define I2C_INTSTAT_SLVPENDING                                                \
-  (1 << 8)                              /* Slave Pending Interrupt Status Bit \
-                                         */
-#define I2C_INTSTAT_SLVNOTSTR (1 << 11) /* Slave not stretching Clock Interrupt Status Bit */
-#define I2C_INTSTAT_SLVDESEL                                                \
-  (1 << 15)                          /* Slave Deselect Interrupt Status Bit \
-                                      */
-#define I2C_INTSTAT_MONRDY (1 << 16) /* Monitor Ready Interrupt Status Bit */
-#define I2C_INTSTAT_MONOV                                                           \
-  (1 << 17)                                /* Monitor Overflow Interrupt Status Bit \
-                                            */
-#define I2C_INTSTAT_MONIDLE (1 << 19)      /* Monitor Idle Interrupt Status Bit */
-#define I2C_INTSTAT_EVENTTIMEOUT (1 << 24) /* Event Timeout Interrupt Status Bit */
-#define I2C_INTSTAT_SCLTIMEOUT                  \
-  (1 << 25) /* SCL Timeout Interrupt Status Bit \
-             */
-
-/* I2C Master Control register Bit definition */
-#define I2C_MSTCTL_MSTCONTINUE (1 << 0) /* Master Continue Bit */
-#define I2C_MSTCTL_MSTSTART (1 << 1)    /* Master Start Control Bit */
-#define I2C_MSTCTL_MSTSTOP (1 << 2)     /* Master Stop Control Bit */
-#define I2C_MSTCTL_MSTDMA (1 << 3)      /* Master DMA Enable Bit */
-
-/* I2C Master Time Register Field definition */
-#define I2C_MSTTIME_MSTSCLLOW (0x07 << 0)  /* Master SCL Low Time field */
-#define I2C_MSTTIME_MSTSCLHIGH (0x07 << 4) /* Master SCL High Time field */
-
-/* I2C Master Data Mask */
-#define I2C_MSTDAT_DATAMASK ((uint32_t)0x00FF << 0) /* Master data mask */
-
-/* I2C Slave Control register Bit definition */
-#define I2C_SLVCTL_SLVCONTINUE (1 << 0) /* Slave Continue Bit */
-#define I2C_SLVCTL_SLVNACK (1 << 1)     /* Slave NACK Bit */
-#define I2C_SLVCTL_SLVDMA (1 << 3)      /* Slave DMA Enable Bit */
-
-/* I2C Slave Data Mask */
-#define I2C_SLVDAT_DATAMASK ((uint32_t)0x00FF << 0) /* Slave data mask */
-
-/* I2C Slave Address register Bit definition */
-#define I2C_SLVADR_SADISABLE (1 << 0)      /* Slave Address n Disable Bit */
-#define I2C_SLVADR_SLVADR (0x7F << 1)      /* Slave Address field */
-#define I2C_SLVADR_MASK ((uint32_t)0x00FF) /* Slave Address Mask */
-
-/* I2C Slave Address Qualifier 0 Register Bit definition */
-#define I2C_SLVQUAL_QUALMODE0 (1 << 0)   /* Slave Qualifier Mode Enable Bit */
-#define I2C_SLVQUAL_SLVQUAL0 (0x7F << 1) /* Slave Qualifier Address for Address 0 */
-
-/* I2C Monitor Data Register Bit definition */
-#define I2C_MONRXDAT_DATA                                                       \
-  (0xFF << 0)                            /* Monitor Function Receive Data Field \
-                                          */
-#define I2C_MONRXDAT_MONSTART (1 << 8)   /* Monitor Received Start Bit */
-#define I2C_MONRXDAT_MONRESTART (1 << 9) /* Monitor Received Repeated Start Bit */
-#define I2C_MONRXDAT_MONNACK (1 << 10)   /* Monitor Received Nack Bit */
+#define I2C_SLVDAT_RESERVED (0xFFFFFF00) /*!< I2C slave data register reserved bits */
 
 #endif
--- inc/nxp/LPC8XX/LPC8XX_i2c_funcs.h
+++ inc/nxp/LPC8XX/LPC8XX_i2c_funcs.h
@@ -30,32 +30,91 @@ LPC800 series common I2C bus registers, defines and functions.
 #ifndef LPC8XX_I2C_FUNCS_H
 #define LPC8XX_I2C_FUNCS_H
 
-static inline void I2cSetClockDiv(I2C_Type *pI2C, uint32_t clkdiv) {
-  if ((clkdiv >= 1) && (clkdiv <= 65536)) {
-    pI2C->CLKDIV = clkdiv - 1;
-  } else {
-    pI2C->CLKDIV = 0;
-  }
+/**
+ * @brief   Set I2C configuration register
+ * @param   peripheral  base address of I2C peripheral
+ * @param   config      I2C peripheral global configuration
+ */
+static inline void i2cSetConfiguration(I2C_Type *peripheral, uint32_t config) {
+  peripheral->CFG = config & ~I2C_CFG_RESERVED;
 }
 
-static inline uint32_t I2cGetClockDiv(I2C_Type *pI2C) {
-  return (pI2C->CLKDIV & 0xFFFF) + 1;
+/**
+ * @brief   Get I2C Status register
+ * @param   peripheral  base address of I2C peripheral
+ */
+static inline uint32_t i2cGetStatus(I2C_Type *peripheral) {
+  return peripheral->STAT & ~I2C_STAT_RESERVED;
 }
 
-static inline void I2cEnableInt(I2C_Type *pI2C, uint32_t intEn) {
-  pI2C->INTENSET = intEn;
+/**
+ * @brief   Set clock division ratio
+ * @param   peripheral  base address of I2C peripheral
+ * @param   divider     divider value, 0 is passthrough
+ */
+static inline void i2cSetClockDivider(I2C_Type *peripheral, uint32_t divider) {
+  peripheral->CLKDIV = divider & ~I2C_CLKDIV_RESERVED;
 }
 
-static inline void I2cDisableInt(I2C_Type *pI2C, uint32_t intClr) {
-  pI2C->INTENCLR = intClr;
+/**
+ * @brief   Set I2C master control register
+ * @param   peripheral  base address of I2C peripheral
+ * @param   control     control bits to set for master mode
+ */
+static inline void i2cSetMasterControl(I2C_Type *peripheral, uint32_t control) {
+  peripheral->MSTCTL = control & ~I2C_MSCTL_RESERVED;
 }
 
-static inline void I2cClearInt(I2C_Type *pI2C, uint32_t intClr) {
-  I2cDisableInt(pI2C, intClr);
+/**
+ * @brief   Set I2C master data register
+ * @param   peripheral  base address of I2C peripheral
+ * @param   data        data to write to I2C master peripheral
+ */
+static inline void i2cSetMasterData(I2C_Type *peripheral, uint32_t data) {
+  peripheral->MSTDAT = data & ~I2C_MSDAT_RESERVED;
 }
 
-static inline uint32_t I2cGetPendingInt(I2C_Type *pI2C) {
-  return pI2C->INTSTAT & ~I2C_INTSTAT_RESERVED;
+/**
+ * @brief   Get I2C master data register
+ * @param   peripheral  base address of I2C peripheral
+ */
+static inline uint32_t i2cGetMasterData(I2C_Type *peripheral) {
+  return peripheral->MSTDAT & ~I2C_MSDAT_RESERVED;
 }
 
+/**
+ * @brief   Set I2C slave control register
+ * @param   peripheral  base address of I2C peripheral
+ * @param   control     control bits to set for slave mode
+ */
+static inline void i2cSetSlaveControl(I2C_Type *peripheral, uint32_t control) {
+  peripheral->SLVCTL = control & ~I2C_SLVCTL_RESERVED;
+}
+
+/**
+ * @brief   Set I2C slave data register
+ * @param   peripheral  base address of I2C peripheral
+ * @param   data        data to write to I2C slave peripheral
+ */
+static inline void i2cSetSlaveData(I2C_Type *peripheral, uint32_t data) {
+  peripheral->SLVDAT = data & ~I2C_SLVDAT_RESERVED;
+}
+
+/**
+ * @brief   Get I2C slave data register
+ * @param   peripheral  base address of I2C peripheral
+ */
+static inline uint32_t i2cGetSlaveData(I2C_Type *peripheral) {
+  return peripheral->SLVDAT & ~I2C_SLVDAT_RESERVED;
+}
+
+/**
+ * @brief   Set I2C slave address match register
+ * @param   peripheral  base address of I2C peripheral
+ * @param   index       Which match register to set
+ * @param   address     I2C address for the slave to respond to
+ */
+static inline void i2cSetSlaveAddress(I2C_Type *peripheral, I2C_SLVADR_INDEX_Type index, uint32_t address) {
+  peripheral->SLVADR[index] = address & ~I2C_SLVADR_RESERVED;
+}
 #endif
