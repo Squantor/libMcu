63770146a88c18ba0429083b924cfb2031c6110a
Squantor <squantmuts@gmail.com>
2022-10-22 11:32:41 +0200

    WIP fleshing out GPIO bank

--- inc/raspberrypi/io_bank0.h
+++ inc/raspberrypi/io_bank0.h
@@ -10,19 +10,32 @@
 
 typedef volatile struct {
   struct {
-    const uint32_t STATUS; /*!< GPIO status */
-    uint32_t CTRL;         /*!< GPIO control */
+    const uint32_t STATUS; /**< GPIO status */
+    uint32_t CTRL;         /**< GPIO control */
   } GPIO[30];
-  uint32_t INTR[4];              /*!< Raw interrupts */
-  uint32_t PROC0_INTE[4];        /*!< Interrupt Enable for proc0 */
-  uint32_t PROC0_INTF[4];        /*!< Interrupt Force for proc0 */
-  const uint32_t PROC0_INTS[4];  /*!< Interrupt status after masking & forcing for proc0 */
-  uint32_t PROC1_INTE[4];        /*!< Interrupt Enable for proc1 */
-  uint32_t PROC1_INTF[4];        /*!< Interrupt Force for proc1 */
-  const uint32_t PROC1_INTS[4];  /*!< Interrupt status after masking & forcing for proc1 */
-  uint32_t DORMANT_WAKE_INTE[4]; /*!< Interrupt Enable for dormant_wake */
-  uint32_t DORMANT_WAKE_INTF[4]; /*!< Interrupt Force for dormant_wake */
-  uint32_t DORMANT_WAKE_INTS[4]; /*!< Interrupt status after masking & forcing for dormant_wake */
+  uint32_t INTR[4];              /**< Raw interrupts */
+  uint32_t PROC0_INTE[4];        /**< Interrupt Enable for proc0 */
+  uint32_t PROC0_INTF[4];        /**< Interrupt Force for proc0 */
+  const uint32_t PROC0_INTS[4];  /**< Interrupt status after masking & forcing for proc0 */
+  uint32_t PROC1_INTE[4];        /**< Interrupt Enable for proc1 */
+  uint32_t PROC1_INTF[4];        /**< Interrupt Force for proc1 */
+  const uint32_t PROC1_INTS[4];  /**< Interrupt status after masking & forcing for proc1 */
+  uint32_t DORMANT_WAKE_INTE[4]; /**< Interrupt Enable for dormant_wake */
+  uint32_t DORMANT_WAKE_INTF[4]; /**< Interrupt Force for dormant_wake */
+  uint32_t DORMANT_WAKE_INTS[4]; /**< Interrupt status after masking & forcing for dormant_wake */
 } IO_BANK0_Type;
+
+#define IO_BANK0_STATUS_RESERVED (0xFAF5CCFF)            /**< GPIO status reserved bits */
+#define IO_BANK0_CTRL_RESERVED (0xCFFCCCE0)              /**< GPIO controlreserved bits */
+#define IO_BANK0_INTR_RESERVED (0x00000000)              /**< Raw interrupts reserved bits */
+#define IO_BANK0_PROC0_INTE_RESERVED (0x00000000)        /**< Interrupt Enable for proc0 reserved bits */
+#define IO_BANK0_PROC0_INTF_RESERVED (0x00000000)        /**< Interrupt Force for proc0 reserved bits */
+#define IO_BANK0_PROC0_INTS_RESERVED (0x00000000)        /**< Interrupt status for proc0 reserved bits */
+#define IO_BANK0_PROC1_INTE_RESERVED (0x00000000)        /**< Interrupt Enable for proc1 reserved bits */
+#define IO_BANK0_PROC1_INTF_RESERVED (0x00000000)        /**< Interrupt Force for proc1 reserved bits */
+#define IO_BANK0_PROC1_INTS_RESERVED (0x00000000)        /**< Interrupt status for proc1 reserved bits */
+#define IO_BANK0_DORMANT_WAKE_INTE_RESERVED (0x00000000) /**< Interrupt Enable for dormant_wake reserved bits */
+#define IO_BANK0_DORMANT_WAKE_INTF_RESERVED (0x00000000) /**< Interrupt Force for dormant_wake reserved bits */
+#define IO_BANK0_DORMANT_WAKE_INTS_RESERVED (0x00000000) /**< Interrupt status reserved bits */
 
 #endif
