1c06ff4a9b6ea860e86f439df96ff0714dc8352f
Squantor <squantmuts@gmail.com>
2022-10-23 14:34:38 +0200

    added register definitions for bank0 pads

--- inc/raspberrypi/pads_bank0.h
+++ inc/raspberrypi/pads_bank0.h
@@ -8,7 +8,15 @@
 #ifndef PADS_BANK0_H
 #define PADS_BANK0_H
 
+/**
+ * @brief
+ *
+ */
 typedef volatile struct {
+  uint32_t VOLTAGE_SELECT; /**< Voltage select. Per bank control */
+  uint32_t GPIO[30];       /**< Pad control register */
+  uint32_t SWCLK;          /**< Pad control register */
+  uint32_t SWD;            /**< Pad control register */
 } PADS_BANK0_Type;
 
 #endif
