e106618638e6064bb2bd0c63c7f685afc68d1430
Squantor <squantmuts@gmail.com>
2022-10-28 09:43:37 +0200

    split up sio into multiple files as it will have a lot of functions

--- inc/raspberrypi/sio.h
+++ inc/raspberrypi/sio.h
@@ -76,64 +76,9 @@ typedef volatile struct {
   uint32_t SPINLOCK[32];       /**< Spinlock registers */
 } SIO_Type;
 
-/**
- * @brief Clear output enable pin
- *
- * @param peripheral  SIO peripheral
- * @param setting     gpio bits to clear
- */
-static inline void sioGpioOeClr(SIO_Type* const peripheral, uint32_t setting) {
-  peripheral->GPIO_OE_CLR = setting;
-}
-
-/**
- * @brief Set output enable pin
- *
- * @param peripheral  SIO peripheral
- * @param setting     gpio bits to set
- */
-static inline void sioGpioOeSet(SIO_Type* const peripheral, uint32_t setting) {
-  peripheral->GPIO_OE_SET = setting;
-}
-
-/**
- * @brief Configure output pins in one go
- *
- * @param peripheral  SIO peripheral
- * @param setting     set all the gpio pins in one go
- */
-static inline void sioGpioOut(SIO_Type* const peripheral, uint32_t setting) {
-  peripheral->GPIO_OUT = setting;
-}
-
-/**
- * @brief set output pins with mask
- *
- * @param peripheral  SIO peripheral
- * @param mask        mask of pins to set
- */
-static inline void sioGpioOutSet(SIO_Type* const peripheral, uint32_t mask) {
-  peripheral->GPIO_OUT_SET = mask;
-}
-
-/**
- * @brief clear output pins with mask
- *
- * @param peripheral  SIO peripheral
- * @param mask        mask of pins to clear
- */
-static inline void sioGpioOutClr(SIO_Type* const peripheral, uint32_t mask) {
-  peripheral->GPIO_OUT_CLR = mask;
-}
-
-/**
- * @brief toggle output pins with mask
- *
- * @param peripheral  SIO peripheral
- * @param mask        mask of pins to toggle
- */
-static inline void sioGpioOutXor(SIO_Type* const peripheral, uint32_t mask) {
-  peripheral->GPIO_OUT_XOR = mask;
-}
+#include "raspberrypi/sio_gpio.h"
+#include "raspberrypi/sio_ipc.h"
+#include "raspberrypi/sio_div.h"
+#include "raspberrypi/sio_interp.h"
 
 #endif
---
+++ inc/raspberrypi/sio_interp.h
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2022 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/** @file Single-Cyle I/O Interpolator functions
+ */
+#ifndef SIO_INTERP_H
+#define SIO_INTERP_H
+
+#endif
