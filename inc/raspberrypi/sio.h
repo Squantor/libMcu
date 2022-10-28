a5423d4a4d9940e1770b8917a22de4c0b43a100b
Squantor <squantmuts@gmail.com>
2022-10-28 09:39:04 +0200

    added output manipulation functions

--- inc/raspberrypi/sio.h
+++ inc/raspberrypi/sio.h
@@ -96,4 +96,44 @@ static inline void sioGpioOeSet(SIO_Type* const peripheral, uint32_t setting) {
   peripheral->GPIO_OE_SET = setting;
 }
 
+/**
+ * @brief Configure output pins in one go
+ *
+ * @param peripheral  SIO peripheral
+ * @param setting     set all the gpio pins in one go
+ */
+static inline void sioGpioOut(SIO_Type* const peripheral, uint32_t setting) {
+  peripheral->GPIO_OUT = setting;
+}
+
+/**
+ * @brief set output pins with mask
+ *
+ * @param peripheral  SIO peripheral
+ * @param mask        mask of pins to set
+ */
+static inline void sioGpioOutSet(SIO_Type* const peripheral, uint32_t mask) {
+  peripheral->GPIO_OUT_SET = mask;
+}
+
+/**
+ * @brief clear output pins with mask
+ *
+ * @param peripheral  SIO peripheral
+ * @param mask        mask of pins to clear
+ */
+static inline void sioGpioOutClr(SIO_Type* const peripheral, uint32_t mask) {
+  peripheral->GPIO_OUT_CLR = mask;
+}
+
+/**
+ * @brief toggle output pins with mask
+ *
+ * @param peripheral  SIO peripheral
+ * @param mask        mask of pins to toggle
+ */
+static inline void sioGpioOutXor(SIO_Type* const peripheral, uint32_t mask) {
+  peripheral->GPIO_OUT_XOR = mask;
+}
+
 #endif
