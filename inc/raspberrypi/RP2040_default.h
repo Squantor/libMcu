a9ca07a010f96369c98713f1ae9747c237febf18
Squantor <squantmuts@gmail.com>
2022-12-28 20:27:52 +0100

    added additional clocking networks for RP2040

--- inc/raspberrypi/RP2040_default.h
+++ inc/raspberrypi/RP2040_default.h
@@ -10,12 +10,15 @@ RP2040 default settings for stuff like clocks etcetera
 #ifndef RP2040_DEFAULT_H
 #define RP2040_DEFAULT_H
 
-#if !defined(CLOCK_XTAL) && !defined(CLOCK_CPU)
+#if !defined(FREQ_XTAL) && !defined(FREQ_CPU)
 #warning default configuration is loaded!
 // default clock configuration
-#define CLOCK_XTAL (12000000u)
-#define CLOCK_CPU (12000000u)
-// TODO: Define the clocking network of the RP2040
+#define FREQ_XTAL (12000000u)       /**< Crystal oscillator frequncy */
+#define FREQ_CPU (12000000u)        /**< CPU frequency */
+#define FREQ_USB (12000000u)        /**< USB frequency */
+#define FREQ_ADC (12000000u)        /**< ADC frequency */
+#define FREQ_RTC (CLOCK_XTAL / 256) /**< RTC frequency */
+#define FREQ_PERI (CLOCK_CPU)       /**< Peripherals frequency */
 #endif
 
 #endif
