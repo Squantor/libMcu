a410bc207beb3417559b232cca55abafadc9cf60
Bart Bilos <squantmuts+git@gmail.com>
2023-04-14 12:34:34 +0200

    polled SPI works

--- inc/nxp/LPC8XX.hpp/LPC84X_spi.hpp
+++ inc/nxp/LPC8XX.hpp/LPC84X_spi.hpp
@@ -10,19 +10,34 @@
 #ifndef LPC84X_SPI_HPP
 #define LPC84X_SPI_HPP
 
+#include <span>
+
 namespace instances {
 namespace spi {
+
+using namespace registers::spi;
 
 /**
  * @brief SPI hardware chip enables
  *
  */
-enum class spiChipEnables : uint16_t {
-  SSEL0 = 0, /**< Hardware chip enable 0 */
-  SSEL1 = 1, /**< Hardware chip enable 1 */
-  SSEL2 = 2, /**< Hardware chip enable 2 */
-  SSEL3 = 3, /**< Hardware chip enable 3 */
+enum class spiChipEnables : uint32_t {
+  SSEL0 = (1 << 16), /**< Hardware chip enable 0 */
+  SSEL1 = (1 << 17), /**< Hardware chip enable 1 */
+  SSEL2 = (1 << 18), /**< Hardware chip enable 2 */
+  SSEL3 = (1 << 19), /**< Hardware chip enable 3 */
 };
+
+/**
+ * @brief XOR operator to use slave selects on uint32_t registers
+ *
+ * @param a register input
+ * @param b slave select to xor
+ * @return uint32_t register setting
+ */
+inline uint32_t operator^(uint32_t a, spiChipEnables b) {
+  return a ^ static_cast<uint32_t>(b);
+}
 
 template <uint32_t base, typename chipEnables>
 struct spi {
@@ -40,7 +55,7 @@ struct spi {
    */
   uint32_t init(uint32_t bitRate) {
     uint32_t actualBitRate = setBitRate(bitRate);
-    regs()->CFG = registers::spi::CFG::ENABLE | registers::spi::CFG::MASTER;
+    regs()->CFG = CFG::ENABLE | CFG::MASTER;
     return actualBitRate;
   }
 
@@ -58,9 +73,87 @@ struct spi {
     regs()->DIV = divider - 1; /**< Divider value is -1 encoded as per datasheet */
     return CLOCK_AHB / divider;
   }
-  // void transmit(chipEnables chipEnable, const uint16_t* txData, uint32_t bitCount, bool lastTransaction){};
-  // void receive(chipEnables chipEnable, uint16_t* rxData, uint32_t bitCount, bool lastTransaction){};
-  // void transceive(chipEnables chipEnable, const uint16_t* txData, uint16_t* rxData, uint32_t bitCount, bool lastTransaction){};
+  /**
+   * @brief Transmit data to SPI
+   *
+   * @param device SPI device to transmit to
+   * @param transmitBuffer data to transmit
+   * @param bitcount amount of bits to transmit
+   * @param lastAction is this the last action? This will disable the chip select
+   */
+  void transmit(chipEnables device, const std::span<uint16_t> transmitBuffer, uint16_t bitcount, bool lastAction) {
+    size_t index = 0;
+    uint32_t baseTransferCommand = (0x000F0000u ^ device) | TXDATCTL::RXIGNORE;  // base transfer command with presets
+    while (bitcount > 16) {
+      regs()->TXDATCTL = baseTransferCommand | TXDATCTL::TXDAT(transmitBuffer[index]) | TXDATCTL::LEN(16);
+      while ((regs()->STAT & STAT::TXRDY) == 0)
+        ;
+      bitcount -= 16;
+      index++;
+    }
+    // process remainder
+    if (lastAction)
+      baseTransferCommand |= TXDATCTL::EOT;
+    regs()->TXDATCTL = baseTransferCommand | TXDATCTL::TXDAT(transmitBuffer[index]) | TXDATCTL::LEN(bitcount);
+    while ((regs()->STAT & STAT::TXRDY) == 0)
+      ;
+  }
+  /**
+   * @brief Receive data from SPI, transmitter is active but transmits zeroes, disable MOSI if you want to float it
+   *
+   * @param device SPI device to receive data from
+   * @param receiveBuffer buffer to put data into
+   * @param bitcount amount of bits to receive
+   * @param lastAction is this the last action? This will disable the chip select
+   */
+  void receive(chipEnables device, std::span<uint16_t> receiveBuffer, uint16_t bitcount, bool lastAction) {
+    size_t index = 0;
+    uint32_t baseTransferCommand = 0x000F0000u ^ device;  // base transfer command with presets
+    while (bitcount > 16) {
+      regs()->TXDATCTL = baseTransferCommand | TXDATCTL::LEN(16);
+      while ((regs()->STAT & STAT::RXRDY) == 0)
+        ;
+      receiveBuffer[index] = RXDAT::RXDAT(regs()->RXDAT);
+      bitcount -= 16;
+      index++;
+    }
+    // process remainder
+    if (lastAction)
+      baseTransferCommand |= TXDATCTL::EOT;
+    regs()->TXDATCTL = baseTransferCommand | TXDATCTL::LEN(bitcount);
+    while ((regs()->STAT & STAT::RXRDY) == 0)
+      ;
+    receiveBuffer[index] = RXDAT::RXDAT(regs()->RXDAT);
+  }
+  /**
+   * @brief Transmit and recieve data via SPI
+   *
+   * @param device SPI device to use
+   * @param transmitBuffer data to transmit
+   * @param receiveBuffer buffer to put data into
+   * @param bitcount amount of bits
+   * @param lastAction is this the last action? This will disable the chip select
+   */
+  void transceive(chipEnables device, const std::span<uint16_t> transmitBuffer, std::span<uint16_t> receiveBuffer,
+                  uint16_t bitcount, bool lastAction) {
+    size_t index = 0;
+    uint32_t baseTransferCommand = 0x000F0000u ^ device;  // base transfer command with presets
+    while (bitcount > 16) {
+      regs()->TXDATCTL = baseTransferCommand | TXDATCTL::TXDAT(transmitBuffer[index]) | TXDATCTL::LEN(16);
+      while ((regs()->STAT & STAT::RXRDY) == 0)
+        ;
+      receiveBuffer[index] = RXDAT::RXDAT(regs()->RXDAT);
+      bitcount -= 16;
+      index++;
+    }
+    // process remainder
+    if (lastAction)
+      baseTransferCommand |= TXDATCTL::EOT;
+    regs()->TXDATCTL = baseTransferCommand | TXDATCTL::TXDAT(transmitBuffer[index]) | TXDATCTL::LEN(bitcount);
+    while ((regs()->STAT & STAT::RXRDY) == 0)
+      ;
+    receiveBuffer[index] = RXDAT::RXDAT(regs()->RXDAT);
+  }
 };
 }  // namespace spi
 }  // namespace instances
--- inc/nxp/LPC8XX.hpp/LPC8XX_spi_regs.hpp
+++ inc/nxp/LPC8XX.hpp/LPC8XX_spi_regs.hpp
@@ -30,18 +30,72 @@ struct registers {
   volatile const uint32_t INTSTAT; /**< SPI Interrupt Status register */
 };
 namespace CFG {
-constexpr inline uint32_t ENABLE = (1 << 0); /*!< SPI enable */
-constexpr inline uint32_t MASTER = (1 << 2); /*!< master mode */
-constexpr inline uint32_t SLAVE = (0 << 2);  /*!< slave mode */
-constexpr inline uint32_t LSBF = (1 << 3);   /*!< LSB First mode enable */
-constexpr inline uint32_t CPHA = (1 << 4);   /*!< Clock phase select */
-constexpr inline uint32_t CPOL = (1 << 5);   /*!< Clock polarity select */
-constexpr inline uint32_t LOOP = (1 << 7);   /*!< Loopback mode enable */
-constexpr inline uint32_t SPOL0 = (1 << 8);  /*!< SSEL0 Polarity select */
-constexpr inline uint32_t SPOL1 = (1 << 9);  /*!< SSEL1 Polarity select */
-constexpr inline uint32_t SPOL2 = (1 << 10); /*!< SSEL2 Polarity select */
-constexpr inline uint32_t SPOL3 = (1 << 11); /*!< SSEL3 Polarity select */
+constexpr inline uint32_t ENABLE = (1 << 0); /**< SPI enable */
+constexpr inline uint32_t MASTER = (1 << 2); /**< master mode */
+constexpr inline uint32_t SLAVE = (0 << 2);  /**< slave mode */
+constexpr inline uint32_t LSBF = (1 << 3);   /**< LSB First mode enable */
+constexpr inline uint32_t CPHA = (1 << 4);   /**< Clock phase select */
+constexpr inline uint32_t CPOL = (1 << 5);   /**< Clock polarity select */
+constexpr inline uint32_t LOOP = (1 << 7);   /**< Loopback mode enable */
+constexpr inline uint32_t SPOL0 = (1 << 8);  /**< SSEL0 Polarity select */
+constexpr inline uint32_t SPOL1 = (1 << 9);  /**< SSEL1 Polarity select */
+constexpr inline uint32_t SPOL2 = (1 << 10); /**< SSEL2 Polarity select */
+constexpr inline uint32_t SPOL3 = (1 << 11); /**< SSEL3 Polarity select */
 }  // namespace CFG
+namespace STAT {
+constexpr inline uint32_t RXRDY = (1 << 0);       /**< Receiver Ready flag. When 1 data available  */
+constexpr inline uint32_t TXRDY = (1 << 1);       /**< Transmitter Ready flag. When 1 data can be written */
+constexpr inline uint32_t RXOV = (1 << 2);        /**< Receiver Overrun interrupt flag. Slave mode only */
+constexpr inline uint32_t TXUR = (1 << 3);        /**< Transmitter Underrun interrupt flag. Slave mode only */
+constexpr inline uint32_t SSA = (1 << 4);         /**< Slave Select Assert. Set when selected. */
+constexpr inline uint32_t SSD = (1 << 5);         /**< Slave Select Deassert. Set when deselected. */
+constexpr inline uint32_t STALLED = (1 << 6);     /**< Stalled status flag. */
+constexpr inline uint32_t ENDTRANSFER = (1 << 7); /**< End Transfer control bit. Force end of current transaction */
+constexpr inline uint32_t MSTIDLE = (1 << 8);     /**< Master idle status flag. Master is fully idle */
+}  // namespace STAT
+namespace RXDAT {
+/**
+ * @brief Format received data from RXDAT register field
+ *
+ * @param registerData RXDAT register data
+ * @return constexpr uint16_t data in RXDAT register
+ */
+constexpr inline uint16_t RXDAT(uint32_t registerData) {
+  return static_cast<uint16_t>(registerData & 0xFFFF);
+}
+constexpr inline uint32_t RXSSEL0_N = (1 << 16); /**< Slave 0 selected for receive transaction, zero is active */
+constexpr inline uint32_t RXSSEL1_N = (1 << 17); /**< Slave 1 selected for receive transaction, zero is active */
+constexpr inline uint32_t RXSSEL2_N = (1 << 18); /**< Slave 2 selected for receive transaction, zero is active */
+constexpr inline uint32_t RXSSEL3_N = (1 << 19); /**< Slave 3 selected for receive transaction, zero is active */
+constexpr inline uint32_t SOT = (1 << 20);       /**< Start of transfer flag. 1 when SSEL is asserted the first time */
+}  // namespace RXDAT
+namespace TXDATCTL {
+/**
+ * @brief Formats data to TXDATCTL register field
+ *
+ * @param data data to put in TXDATCTL register
+ * @return constexpr uint32_t returns formatted data
+ */
+constexpr inline uint32_t TXDAT(uint16_t data) {
+  return static_cast<uint32_t>(data);
+}
+constexpr inline uint32_t TXSSEL0_N = (1 << 16); /**< This field asserts SSEL0 in master mode.*/
+constexpr inline uint32_t TXSSEL1_N = (1 << 17); /**< This field asserts SSEL1 in master mode.*/
+constexpr inline uint32_t TXSSEL2_N = (1 << 18); /**< This field asserts SSEL2 in master mode.*/
+constexpr inline uint32_t TXSSEL3_N = (1 << 19); /**< This field asserts SSEL3 in master mode.*/
+constexpr inline uint32_t EOT = (1 << 20);       /**< End of Transfer. Deassert SSEL */
+constexpr inline uint32_t EOF = (1 << 21);       /**< End of Frame. Insert delay between frames */
+constexpr inline uint32_t RXIGNORE = (1 << 22);  /**< Receive ignore */
+/**
+ * @brief Format data transmit length to TXDATCTL register field
+ *
+ * @param length amount of data to transmit
+ * @return constexpr uint32_t returns formatted data
+ */
+constexpr inline uint32_t LEN(uint32_t length) {
+  return ((length - 1) & 0x0F) << 24;
+}
+}  // namespace TXDATCTL
 }  // namespace spi
 }  // namespace registers
 
