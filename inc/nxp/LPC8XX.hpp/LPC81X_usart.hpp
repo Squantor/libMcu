601d05e341395a6d1ebb1ba769f9535b581051a4
Bart Bilos <squantmuts+git@gmail.com>
2023-09-15 10:19:58 +0200

    working API

--- inc/nxp/LPC8XX.hpp/LPC81X_usart.hpp
+++ inc/nxp/LPC8XX.hpp/LPC81X_usart.hpp
@@ -46,6 +46,28 @@ enum uartStop : uint32_t {
   STOP_2 = (1 << 6), /**< 2 stop bits */
 };
 
+/**
+ * @brief Uart status bits
+ *
+ * These bit patterns match the USART STAT register settings
+ */
+enum uartStatus : uint32_t {
+  RXRDY = STAT::RXRDY,               /**< Receiver ready flag, Read only */
+  RXIDLE = STAT::RXIDLE,             /**< Receiver idle, Read only */
+  TXRDY = STAT::TXRDY,               /**< Transmitter ready, Read only  */
+  TXIDLE = STAT::TXIDLE,             /**< Transmitter idle, Read only */
+  CTS = STAT::CTS,                   /**< CTS signal state, Read only */
+  DELTACTS = STAT::DELTACTS,         /**< Change detected in CTS signal, write 1 clear */
+  TXDISINT = STAT::TXDISINT,         /**< Transmitter disabled confirmation, read only  */
+  OVERRUNINT = STAT::OVERRUNINT,     /**< Overrun error interrupt flag, write 1 clear */
+  RXBRK = STAT::RXBRK,               /**< Received break, read only */
+  DELTARXBRK = STAT::DELTARXBRK,     /**< Change detected in receiver break, write 1 clear */
+  START = STAT::START,               /**< Start condition detected, write 1 clear */
+  FRAMERRINT = STAT::FRAMERRINT,     /**< Frame error interrupt flag, write 1 clear */
+  PARITYERRINT = STAT::PARITYERRINT, /**< Parity error interrupt flag, write 1 clear */
+  RXNOISEINT = STAT::RXNOISEINT,     /**< Recieved noise interrupt flag, write 1 clear*/
+};
+
 template <uint32_t base>
 struct usart {
   /**
@@ -64,10 +86,10 @@ struct usart {
    * @return uint32_t actual baud rate
    */
   uint32_t init(uint32_t baudRate) {
-    uint32_t baudDivider = CLOCK_AHB / (baudRate * 16);
+    uint32_t baudDivider = CLOCK_MAIN / (baudRate * 16);
     regs()->BRG = baudDivider;
     regs()->CFG = CFG::ENABLE | uartLength::SIZE_8 | uartParity::NONE | uartStop::STOP_1;
-    return CLOCK_AHB / 16 / baudDivider;
+    return CLOCK_MAIN / 16 / baudDivider;
   }
 
   /**
@@ -80,10 +102,48 @@ struct usart {
    * @return uint32_t actual baud rate
    */
   uint32_t init(uint32_t baudRate, uartLength lengthBits, uartParity parity, uartStop stopBits) {
-    uint32_t baudDivider = CLOCK_AHB / (baudRate * 16);
+    uint32_t baudDivider = CLOCK_MAIN / (baudRate * 16);
     regs()->BRG = baudDivider;
     regs()->CFG = CFG::ENABLE | lengthBits | parity | stopBits;
-    return CLOCK_AHB / 16 / baudDivider;
+    return CLOCK_MAIN / 16 / baudDivider;
+  }
+
+  /**
+   * @brief return uart status
+   *
+   * @return uint32_t one to one copy of the status register, see bit masks for options
+   */
+  uint32_t status() {
+    return regs()->STAT & STAT::MASK;
+  }
+
+  /**
+   * @brief Send data out of the UART
+   *
+   * @param data data to send, amount is sent according to configuration
+   */
+  void write(uint32_t data) {
+    regs()->TXDAT = data & TXDAT::MASK;
+  }
+  /**
+   * @brief Read data from UART
+   *
+   * @param data reference to put received data in
+   */
+  void read(uint32_t &data) {
+    data = regs()->RXDAT;
+  }
+
+  /**
+   * @brief Read data and status from UART
+   *
+   * @param data reference to put received data in
+   * @param status reference to put received status in
+   */
+  void read(uint32_t &data, uint32_t &status) {
+    uint32_t regData = regs()->RXDATSTAT;
+    data = regData & RXDATSTAT::MASK_DATA;
+    status = regData & RXDATSTAT::MASK_STAT;
   }
 };
 }  // namespace usart
--- inc/nxp/LPC8XX.hpp/LPC81X_usart_regs.hpp
+++ inc/nxp/LPC8XX.hpp/LPC81X_usart_regs.hpp
@@ -45,7 +45,21 @@ constexpr inline uint32_t MASK = 0x00000000; /**< register mask for allowed bits
 }  // namespace CTL
 namespace STAT {
 // TODO register definitions
-constexpr inline uint32_t MASK = 0x00000000; /**< register mask for allowed bits */
+constexpr inline uint32_t MASK = 0x0000FD7F;        /**< Register mask for allowed bits, Read only */
+constexpr inline uint32_t RXRDY = (1 << 0);         /**< Receiver ready flag, Read only */
+constexpr inline uint32_t RXIDLE = (1 << 1);        /**< Receiver idle, Read only */
+constexpr inline uint32_t TXRDY = (1 << 2);         /**< Transmitter ready, Read only  */
+constexpr inline uint32_t TXIDLE = (1 << 3);        /**< Transmitter idle, Read only */
+constexpr inline uint32_t CTS = (1 << 4);           /**< CTS signal state, Read only */
+constexpr inline uint32_t DELTACTS = (1 << 5);      /**< Change detected in CTS signal, write 1 clear */
+constexpr inline uint32_t TXDISINT = (1 << 6);      /**< Transmitter disabled confirmation, read only  */
+constexpr inline uint32_t OVERRUNINT = (1 << 8);    /**< Overrun error interrupt flag, write 1 clear */
+constexpr inline uint32_t RXBRK = (1 << 10);        /**< Received break, read only */
+constexpr inline uint32_t DELTARXBRK = (1 << 11);   /**< Change detected in receiver break, write 1 clear */
+constexpr inline uint32_t START = (1 << 12);        /**< Start condition detected, write 1 clear */
+constexpr inline uint32_t FRAMERRINT = (1 << 13);   /**< Frame error interrupt flag, write 1 clear */
+constexpr inline uint32_t PARITYERRINT = (1 << 14); /**< Parity error interrupt flag, write 1 clear */
+constexpr inline uint32_t RXNOISEINT = (1 << 15);   /**< Recieved noise interrupt flag, write 1 clear*/
 }  // namespace STAT
 namespace INTENSET {
 // TODO register definitions
@@ -57,15 +71,20 @@ constexpr inline uint32_t MASK = 0x00000000; /**< register mask for allowed bits
 }  // namespace INTENCLR
 namespace RXDAT {
 // TODO register definitions
-constexpr inline uint32_t MASK = 0x00000000; /**< register mask for allowed bits */
+constexpr inline uint32_t MASK = 0x000001FF; /**< register mask for allowed bits */
 }  // namespace RXDAT
 namespace RXDATSTAT {
 // TODO register definitions
-constexpr inline uint32_t MASK = 0x00000000; /**< register mask for allowed bits */
+constexpr inline uint32_t MASK = 0x000071FF;      /**< register mask for allowed bits */
+constexpr inline uint32_t MASK_DATA = 0x000000FF; /**< register mask for received data */
+constexpr inline uint32_t MASK_STAT = 0x00007000; /**< register mask for status bits */
+constexpr inline uint32_t FRAMEERR = (1 << 13);   /**< Framing error status flag */
+constexpr inline uint32_t PARITYERR = (1 << 14);  /**< Parity error status flag */
+constexpr inline uint32_t RXNOISE = (1 << 15);    /**< Recieved noise flag */
 }  // namespace RXDATSTAT
 namespace TXDAT {
 // TODO register definitions
-constexpr inline uint32_t MASK = 0x00000000; /**< register mask for allowed bits */
+constexpr inline uint32_t MASK = 0x000001FF; /**< register mask for allowed bits */
 }  // namespace TXDAT
 namespace BRG {
 // TODO register definitions
