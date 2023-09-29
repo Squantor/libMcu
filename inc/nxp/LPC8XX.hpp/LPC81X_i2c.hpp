e7be899c120151180f818ff468a177c867e3cad4
Bart Bilos <squantmuts+git@gmail.com>
2023-09-29 13:27:26 +0200

    Added I2C init

--- inc/nxp/LPC8XX.hpp/LPC81X_i2c.hpp
+++ inc/nxp/LPC8XX.hpp/LPC81X_i2c.hpp
@@ -23,6 +23,19 @@ struct i2c {
   static auto regs() {
     return reinterpret_cast<registers::i2c::registers *>(base);
   }
+
+  /**
+   * @brief Initialize I2C master
+   *
+   * @param bitRate requested bit rate
+   * @return uint32_t actual bit rate
+   */
+  uint32_t initMaster(uint32_t bitRate) {
+    uint32_t divider = CLOCK_AHB / bitRate;
+    regs()->CLKDIV = divider + 1;
+    regs()->CFG = CFG::MSTEN;
+    return CLOCK_AHB / divider;
+  }
 };
 }  // namespace i2c
 }  // namespace instances
