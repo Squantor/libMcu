bfa755b278c8a646458a850b35563ed2b2f53533
Bart Bilos <squantmuts+git@gmail.com>
2024-01-26 08:39:32 +0100

    added flash control method

--- inc/nxp/LPC8XX.hpp/LPC81X_fmc_sw.hpp
+++ inc/nxp/LPC8XX.hpp/LPC81X_fmc_sw.hpp
@@ -14,9 +14,20 @@ namespace libMcuLL {
 namespace sw {
 namespace fmc {
 using namespace libMcuLL::hw::fmc;
+
+enum class waitstates : std::uint32_t {
+  WAIT_1_CLOCK = FLASHCFG::FLASHTIM_1_CLOCK, /**< 1 clock flash waitstate */
+  WAIT_2_CLOCK = FLASHCFG::FLASHTIM_2_CLOCK, /**< 2 clock flash waitstate */
+};
+
 template <libMcuLL::FMCbaseAddress address_>
 struct fmc {
-  static constexpr libMcuLL::hwAddressType address = address_; /**< peripheral address */
+  /**
+   * @brief Construct a new fmc object
+   *
+   */
+  fmc() {}
+
   /**
    * @brief get registers from peripheral
    *
@@ -25,6 +36,22 @@ struct fmc {
   static hw::fmc::peripheral *peripheral() {
     return reinterpret_cast<hw::fmc::peripheral *>(address_);
   }
+
+  /**
+   * @brief Change flash controller wait states
+   *
+   *
+   *
+   * @param setting amount of waitstates to wait for accessing flash
+   */
+  constexpr void setFlashWaitState(waitstates setting) {
+    // reserved bits need to be EXACTLY written back according to datasheet
+    peripheral()->FLASHCFG = (peripheral()->FLASHCFG & ~FLASHCFG::RESERVED_MASK) | static_cast<std::uint32_t>(setting);
+  }
+
+  // TODO flash signiature generator
+
+  static constexpr libMcuLL::hwAddressType address = address_; /**< peripheral address */
 };
 }  // namespace fmc
 }  // namespace sw
