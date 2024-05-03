d2475d93fbdaf75598527178db4c9fe646ab4a5f
Bart Bilos <squantmuts+git@gmail.com>
2024-05-03 10:32:28 +0200

    fill some boilerplate code in here

---
+++ inc/drivers/dummy.hpp
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2024 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/**
+ * \file Empty driver dummy file
+ */
+#ifndef DRIVER_DUMMY_HPP
+#define DRIVER_DUMMY_HPP
+
+#endif
