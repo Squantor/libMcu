0e3675cd04a573b6f13f4ce3c5ef0d41e8fcf5ac
Bart Bilos <squantmuts+git@gmail.com>
2024-05-14 12:17:18 +0200

    added all functionality

--- inc/drivers/mux3to8.hpp
+++ inc/drivers/mux3to8.hpp
@@ -14,20 +14,58 @@ namespace libMcuDriver::mux {
 
 using namespace libMcuHal;
 // template <auto &gpioPeripheral, auto &notEnablePin, auto &A0Pin>
-template <auto &gpioPeripheral>
+template <auto &gpioPeripheral, typename notEnablePinType, typename a0PinType, typename a1PinType, typename a2PinType>
 struct mux3to8 {
+  /**
+   * @brief
+   *
+   */
   void initialize() {
-    gpioPeripheral.initialize();
+    gpioPeripheral.high(notEnablePin);
+    gpioPeripheral.output(notEnablePin);
+    gpioPeripheral.output(a0Pin);
+    gpioPeripheral.output(a1Pin);
+    gpioPeripheral.output(a2Pin);
+  }
+
+  /**
+   * @brief
+   *
+   * @param enable
+   * @param value
+   */
+  void set(bool enable, std::uint32_t value) {
+    if (value & 0x01)
+      gpioPeripheral.high(a0Pin);
+    else
+      gpioPeripheral.low(a0Pin);
+    if (value & 0x02)
+      gpioPeripheral.high(a1Pin);
+    else
+      gpioPeripheral.low(a1Pin);
+    if (value & 0x04)
+      gpioPeripheral.high(a2Pin);
+    else
+      gpioPeripheral.low(a2Pin);
+    if (enable)
+      gpioPeripheral.low(notEnablePin);
+    else
+      gpioPeripheral.high(notEnablePin);
   }
 
  private:
+  static constexpr notEnablePinType notEnablePin{};
+  static constexpr a0PinType a0Pin{};
+  static constexpr a1PinType a1Pin{};
+  static constexpr a2PinType a2Pin{};
+
   // add constraints here
   using peripheralType = std::remove_reference<decltype(gpioPeripheral)>::type;
-  // using notEnablePinType = std::remove_reference<decltype(notEnablePin)>::type;
-  // using A0PinType = std::remove_reference<decltype(A0Pin)>::type;
-  static_assert(std::is_base_of<libMcuHal::halGpioBase, peripheralType>::value, "gpioPeripheral is not a Hal Gpio Class");
-  // static_assert(std::is_base_of<libMcu::pinBase, notEnablePinType>::value, "gpioPeripheral is not a Hal Gpio Class");
-  // static_assert(std::is_base_of<libMcu::pinBase, A0PinType>::value, "gpioPeripheral is not a Hal Gpio Class");
+  static_assert(std::is_base_of<libMcuHal::halGpioBase, peripheralType>::value, "gpioPeripheral is not derived from halGpioBase");
+  static_assert(std::is_base_of<libMcu::pinBase, notEnablePinType>::value, "notEnablePinType is not derived from pinBase");
+  static_assert(std::is_base_of<libMcu::pinBase, a0PinType>::value, "a0PinType is not derived from pinBase");
+  static_assert(std::is_base_of<libMcu::pinBase, a1PinType>::value, "a1PinType is not derived from pinBase");
+  static_assert(std::is_base_of<libMcu::pinBase, a2PinType>::value, "a2PinType is not derived from pinBase");
 };
 }  // namespace libMcuDriver::mux
 
