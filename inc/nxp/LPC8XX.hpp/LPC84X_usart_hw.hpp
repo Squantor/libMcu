3a1ebea74da5b8973ec1069fc0960492af3db72c
Bart Bilos <squantmuts+git@gmail.com>
2024-08-02 12:27:41 +0200

    added usart library

--- inc/nxp/LPC8XX.hpp/LPC84X_usart_hw.hpp
+++ inc/nxp/LPC8XX.hpp/LPC84X_usart_hw.hpp
@@ -28,5 +28,77 @@ struct peripheral {
   volatile std::uint32_t OSR;             /**< Oversample selection register for asynchronous communication */
   volatile std::uint32_t ADDR;            /**< Address register for automatic address matching */
 };
+namespace CFG {
+constexpr inline std::uint32_t RESERVED_MASK{0x00FC'DA7Du}; /**< register mask for allowed bits */
+constexpr inline std::uint32_t ENABLE{1u << 0};             /**< Enable USART */
+constexpr inline std::uint32_t DATALEN7BIT{0u << 2};        /**< 7 data bits */
+constexpr inline std::uint32_t DATALEN8BIT{1u << 2};        /**< 8 data bits */
+constexpr inline std::uint32_t DATALEN9BIT{2u << 2};        /**< 9 data bits */
+constexpr inline std::uint32_t PARITY_NONE{0u << 4};        /**< No parity */
+constexpr inline std::uint32_t PARITY_EVEN{2u << 4};        /**< Even parity */
+constexpr inline std::uint32_t PARITY_ODD{3u << 4};         /**< Odd parity */
+constexpr inline std::uint32_t STOPBIT1{0u << 6};           /**< 1 stop bits */
+constexpr inline std::uint32_t STOPBIT2{1u << 6};           /*<< 2 stop bits */
+constexpr inline std::uint32_t CTSEN{1u << 9};              /**< CTS enable */
+constexpr inline std::uint32_t SYNCEN{1u << 11};            /**< Synchronous enable */
+constexpr inline std::uint32_t CLKPOL{1u << 12};            /**< Rising edge sampling */
+constexpr inline std::uint32_t SYNCMST{1u << 14};           /**< Synchronous master selected */
+constexpr inline std::uint32_t LOOP{1u << 15};              /**< Loopback mode */
+}  // namespace CFG
+namespace CTL {
+constexpr inline std::uint32_t RESERVED_MASK{0x0001'0346u}; /**< register mask for allowed bits */
+constexpr inline std::uint32_t TXDIS{1u << 6};              /**< Disable transmitter */
+constexpr inline std::uint32_t AUTOBAUD{1u << 16};          /**< Enable autobaud */
+}  // namespace CTL
+namespace STAT {
+constexpr inline std::uint32_t RESERVED_MASK{0x0001'FD7Fu}; /**< Register mask for allowed bits, Read only */
+constexpr inline std::uint32_t RXRDY{1u << 0};              /**< Receiver ready flag, Read only */
+constexpr inline std::uint32_t RXIDLE{1u << 1};             /**< Receiver idle, Read only */
+constexpr inline std::uint32_t TXRDY{1u << 2};              /**< Transmitter ready, Read only  */
+constexpr inline std::uint32_t TXIDLE{1u << 3};             /**< Transmitter idle, Read only */
+constexpr inline std::uint32_t CTS{1u << 4};                /**< CTS signal state, Read only */
+constexpr inline std::uint32_t DELTACTS{1u << 5};           /**< Change detected in CTS signal, write 1 clear */
+constexpr inline std::uint32_t TXDISINT{1u << 6};           /**< Transmitter disabled confirmation, read only  */
+constexpr inline std::uint32_t OVERRUNINT{1u << 8};         /**< Overrun error interrupt flag, write 1 clear */
+constexpr inline std::uint32_t RXBRK{1u << 10};             /**< Received break, read only */
+constexpr inline std::uint32_t DELTARXBRK{1u << 11};        /**< Change detected in receiver break, write 1 clear */
+constexpr inline std::uint32_t START{1u << 12};             /**< Start condition detected, write 1 clear */
+constexpr inline std::uint32_t FRAMERRINT{1u << 13};        /**< Frame error interrupt flag, write 1 clear */
+constexpr inline std::uint32_t PARITYERRINT{1u << 14};      /**< Parity error interrupt flag, write 1 clear */
+constexpr inline std::uint32_t RXNOISEINT{1u << 15};        /**< Recieved noise interrupt flag, write 1 clear*/
+constexpr inline std::uint32_t ABERR{1u << 16};             /**< Autobaud error, write 1 clears */
+}  // namespace STAT
+namespace INTENSET {
+constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
+}  // namespace INTENSET
+namespace INTENCLR {
+constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
+}  // namespace INTENCLR
+namespace RXDAT {
+constexpr inline std::uint32_t RESERVED_MASK{0x0000'01FFu}; /**< register mask for allowed bits */
+}  // namespace RXDAT
+namespace RXDATSTAT {
+constexpr inline std::uint32_t RESERVED_MASK{0x0000'71FFu}; /**< register mask for allowed bits */
+constexpr inline std::uint32_t DATA_MASK{0x0000'00FFu};     /**< register mask for received data */
+constexpr inline std::uint32_t STAT_MASK{0x0000'7000u};     /**< register mask for status bits */
+constexpr inline std::uint32_t FRAMEERR{1u << 13};          /**< Framing error status flag */
+constexpr inline std::uint32_t PARITYERR{1u << 14};         /**< Parity error status flag */
+constexpr inline std::uint32_t RXNOISE{1u << 15};           /**< Recieved noise flag */
+}  // namespace RXDATSTAT
+namespace TXDAT {
+constexpr inline std::uint32_t RESERVED_MASK{0x0000'01FFu}; /**< register mask for allowed bits */
+}  // namespace TXDAT
+namespace BRG {
+constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
+}  // namespace BRG
+namespace INTSTAT {
+constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
+}  // namespace INTSTAT
+namespace OSR {
+constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
+}  // namespace OSR
+namespace ADDR {
+constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
+}  // namespace ADDR
 }  // namespace libMcu::hw::usart
 #endif
