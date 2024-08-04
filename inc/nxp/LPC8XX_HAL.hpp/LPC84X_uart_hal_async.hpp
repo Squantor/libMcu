e7d560d9495934b6f6ec653ae95b3b7359698af2
Bart Bilos <squantmuts+git@gmail.com>
2024-08-04 20:38:43 +0200

    initial start for uart async hal class

---
+++ inc/nxp/LPC8XX_HAL.hpp/LPC84X_uart_hal_async.hpp
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2024 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/**
+ * \file LPC840 series asynchronous UART HAL
+ */
+#ifndef LPC84X_UART_HAL_ASYNC_HPP
+#define LPC84X_UART_HAL_ASYNC_HPP
+#endif
