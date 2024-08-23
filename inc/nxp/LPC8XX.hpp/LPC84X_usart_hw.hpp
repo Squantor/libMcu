a785b36d1d1e18d8d1ae9ed781691d22af038b33
Bart Bilos <squantmuts+git@gmail.com>
2024-08-23 10:55:41 +0200

    added interrupt enable/clear/status flag definitions

--- inc/nxp/LPC8XX.hpp/LPC84X_usart_hw.hpp
+++ inc/nxp/LPC8XX.hpp/LPC84X_usart_hw.hpp
@@ -69,10 +69,34 @@ constexpr inline std::uint32_t RXNOISEINT{1u << 15};        /**< Recieved noise 
 constexpr inline std::uint32_t ABERR{1u << 16};             /**< Autobaud error, write 1 clears */
 }  // namespace STAT
 namespace INTENSET {
-constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
+constexpr inline std::uint32_t RESERVED_MASK{0x0001'F96Du}; /**< Register mask for allowed bits */
+constexpr inline std::uint32_t RXRDYEN{1u << 0};            /**< Data ready in RXDAT interrupt enable */
+constexpr inline std::uint32_t TXRDYEN{1u << 2};            /**< TXDAT availble for write interrupt enable */
+constexpr inline std::uint32_t TXIDLEEN{1u << 3};           /**< Transmitter idle interrupt enable*/
+constexpr inline std::uint32_t DELTACTSEN{1u << 5};         /**< Change in CTS interrupt enable*/
+constexpr inline std::uint32_t TXDISEN{1u << 6};            /**< Transmit fully disabled interrupt enable*/
+constexpr inline std::uint32_t OVERRUNEN{1u << 8};          /**< Overrun error interrupt enable*/
+constexpr inline std::uint32_t DELTARXBRKEN{1u << 11};      /**< Change in RX break condition interrupt enable*/
+constexpr inline std::uint32_t STARTEN{1u << 12};           /**< Start bit detected interrupt enable*/
+constexpr inline std::uint32_t FRAMERREN{1u << 13};         /**< Framing error interrupt enable*/
+constexpr inline std::uint32_t PARITYERREN{1u << 14};       /**< Parity error interrupt enable*/
+constexpr inline std::uint32_t RXNOISEEN{1u << 15};         /**< RX noise detected interrupt enable*/
+constexpr inline std::uint32_t ABERREN{1u << 16};           /**< Autobaud error interrupt enable*/
 }  // namespace INTENSET
 namespace INTENCLR {
-constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
+constexpr inline std::uint32_t RESERVED_MASK{0x0001'F96Du}; /**< register mask for allowed bits */
+constexpr inline std::uint32_t RXRDYCLR{1u << 0};           /**< Data ready in RXDAT interrupt clear */
+constexpr inline std::uint32_t TXRDYCLR{1u << 2};           /**< TXDAT availble for write interrupt clear */
+constexpr inline std::uint32_t TXIDLECLR{1u << 3};          /**< Transmitter idle interrupt clear */
+constexpr inline std::uint32_t DELTACTSCLR{1u << 5};        /**< Change in CTS interrupt clear */
+constexpr inline std::uint32_t TXDISINTCLR{1u << 6};        /**< Transmit fully disabled interrupt clear */
+constexpr inline std::uint32_t OVERRUNCLR{1u << 8};         /**< Overrun error interrupt clear */
+constexpr inline std::uint32_t DELTARXBRKCLR{1u << 11};     /**< Change in RX break condition interrupt clear */
+constexpr inline std::uint32_t STARTCLR{1u << 12};          /**< Start bit detected interrupt clear */
+constexpr inline std::uint32_t FRAMERRCLR{1u << 13};        /**< Framing error interrupt clear */
+constexpr inline std::uint32_t PARITYERRCLR{1u << 14};      /**< Parity error interrupt clear */
+constexpr inline std::uint32_t RXNOISECLR{1u << 15};        /**< RX noise detected interrupt clear */
+constexpr inline std::uint32_t ABERRCLR{1u << 16};          /**< Autobaud error interrupt clear */
 }  // namespace INTENCLR
 namespace RXDAT {
 constexpr inline std::uint32_t RESERVED_MASK{0x0000'01FFu}; /**< register mask for allowed bits */
@@ -92,7 +116,19 @@ namespace BRG {
 constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
 }  // namespace BRG
 namespace INTSTAT {
-constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
+constexpr inline std::uint32_t RESERVED_MASK{0x0001'F96Du}; /**< register mask for allowed bits */
+constexpr inline std::uint32_t RXRDY{1u << 0};              /**< Data ready in RXDAT interrupt */
+constexpr inline std::uint32_t TXRDY{1u << 2};              /**< TXDAT availble for write interrupt */
+constexpr inline std::uint32_t TXIDLE{1u << 3};             /**< Transmitter idle interrupt */
+constexpr inline std::uint32_t DELTACTS{1u << 5};           /**< Change in CTS interrupt */
+constexpr inline std::uint32_t TXDISINT{1u << 6};           /**< Transmit fully disabled interrupt */
+constexpr inline std::uint32_t OVERRUNINT{1u << 8};         /**< Overrun error interrupt */
+constexpr inline std::uint32_t DELTARXBRK{1u << 11};        /**< Change in RX break condition interrupt */
+constexpr inline std::uint32_t START{1u << 12};             /**< Start bit detected interrupt */
+constexpr inline std::uint32_t FRAMERRINT{1u << 13};        /**< Framing error interrupt */
+constexpr inline std::uint32_t PARITYERRINT{1u << 14};      /**< Parity error interrupt */
+constexpr inline std::uint32_t RXNOISEINT{1u << 15};        /**< RX noise detected interrupt */
+constexpr inline std::uint32_t ABERR{1u << 16};             /**< Autobaud error interrupt */
 }  // namespace INTSTAT
 namespace OSR {
 constexpr inline std::uint32_t RESERVED_MASK{0x0000'0000u}; /**< register mask for allowed bits */
