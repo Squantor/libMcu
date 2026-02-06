2c62ce2c7a62de66339254be468c4bd22032cb3d
Bart Bilos <squantmuts+git@gmail.com>
2026-02-06 10:33:30 +0100

    added bitmap class and blit function

---
+++ inc/libmcu/bitmap/bitmap.hpp
@@ -0,0 +1,106 @@
+/*
+SPDX-License-Identifier: MIT
+
+Copyright (c) 2026 Bart Bilos
+For conditions of distribution and use, see LICENSE file
+*/
+/**
+ * \file bitmap.cpp
+ * \brief Class definitions for bitmap class
+ */
+#ifndef BITMAP_HPP
+#define BITMAP_HPP
+
+#include <cstdint>
+#include <type_traits>
+
+namespace detail {}
+
+/**
+ * @brief Class for representing a bitmap
+ * @todo Change the data to a std::span
+ * @tparam T
+ */
+template <typename T>
+class Bitmap_view {
+ public:
+  using Pixel_type = T;
+  constexpr Bitmap_view(Pixel_type *bitmap_data, std::uint16_t bitmap_width, std::uint16_t bitmap_height,
+                        std::uint8_t bitmap_bits_per_pixel)
+    : data_bitmap(bitmap_data), width(bitmap_width), height(bitmap_height), bits_per_pixel(bitmap_bits_per_pixel) {}
+
+  [[nodiscard]] constexpr uint16_t get_width() const {
+    return width;
+  }
+  [[nodiscard]] constexpr uint16_t get_height() const {
+    return height;
+  }
+  [[nodiscard]] constexpr std::uint8_t get_bits_per_pixel() const {
+    return bits_per_pixel;
+  }
+  [[nodiscard]] constexpr Pixel_type *data() const {
+    return data_bitmap;
+  }
+
+  constexpr void fill(Pixel_type pixel)
+  requires(!std::is_const_v<Pixel_type>)
+  {
+    Pixel_type fill_value = pixel;
+    std::size_t fill_count = get_bits_per_pixel_type() / bits_per_pixel;
+    for (std::size_t count = 0; count < fill_count; count++) {
+      fill_value = fill_value | static_cast<Pixel_type>(fill_value << bits_per_pixel);
+    }
+    std::size_t max_bitmap_index = (width * height * bits_per_pixel) / get_bits_per_pixel_type();
+    for (std::size_t i = 0; i < max_bitmap_index; i++) {
+      data_bitmap[i] = fill_value;
+    }
+  }
+
+  [[nodiscard]] constexpr Bitmap_view<const T> as_const() const {
+    return Bitmap_view<const T>{data_bitmap, width, height, bits_per_pixel};
+  }
+
+  [[nodiscard]] constexpr Pixel_type get_pixel(std::uint16_t x, std::uint16_t y) const {
+    if (x >= width || y >= height)
+      return 0;
+    using Pixel_type_nonconst = std::remove_const_t<Pixel_type>;
+    std::size_t pixel_index = (y * width) + x;
+    std::size_t bit_index = pixel_index * bits_per_pixel;
+    std::size_t data_index = bit_index / get_bits_per_pixel_type();
+    std::size_t lsb_shift = bit_index % get_bits_per_pixel_type();
+    std::size_t msb_shift = (get_bits_per_pixel_type() - lsb_shift) - bits_per_pixel;
+    Pixel_type_nonconst mask = ~0;
+    mask = static_cast<Pixel_type_nonconst>(mask >> msb_shift);
+    Pixel_type_nonconst pixel = static_cast<Pixel_type_nonconst>((data_bitmap[data_index] & mask) >> lsb_shift);
+    return pixel;
+  }
+
+  constexpr void set_pixel(std::uint16_t x, std::uint16_t y, Pixel_type pixel)
+  requires(!std::is_const_v<Pixel_type>)
+  {
+    if (x >= width || y >= height)
+      return;
+    std::size_t pixel_index = (y * width) + x;
+    std::size_t bit_index = pixel_index * bits_per_pixel;
+    std::size_t data_index = bit_index / get_bits_per_pixel_type();
+    std::size_t lsb_shift = bit_index % get_bits_per_pixel_type();
+    std::size_t msb_shift = (get_bits_per_pixel_type() - lsb_shift) - bits_per_pixel;
+    Pixel_type mask = ~0;
+    mask = static_cast<Pixel_type>(mask >> msb_shift << lsb_shift);
+    data_bitmap[data_index] = static_cast<Pixel_type>((data_bitmap[data_index] & ~mask) | (pixel << lsb_shift));
+  }
+
+ private:
+  [[nodiscard]] constexpr std::size_t get_bits_per_pixel_type() const {
+    return (sizeof(Pixel_type) * 8);
+  }
+  Pixel_type *data_bitmap;
+  std::uint16_t width;
+  std::uint16_t height;
+  std::uint8_t bits_per_pixel;
+};
+
+using Bitmap = Bitmap_view<std::uint32_t>;
+using Const_bitmap = Bitmap_view<const std::uint32_t>;
+
+#endif  // BITMAP_HPP
---
+++ inc/libmcu/bitmap/blit.hpp
@@ -0,0 +1,25 @@
+/*
+SPDX-License-Identifier: MIT
+
+Copyright (c) 2026 Bart Bilos
+For conditions of distribution and use, see LICENSE file
+*/
+/**
+ * \file blit.hpp
+ * \brief Function definitions for Blitting functions
+ */
+#ifndef BLIT_HPP
+#define BLIT_HPP
+
+#include <libmcu/bitmap/bitmap.hpp>
+
+template <typename Src_pixel, typename Dst_pixel>
+constexpr void blit(Bitmap_view<const Src_pixel> src, Bitmap_view<Dst_pixel> dst, std::uint16_t dst_x, std::uint16_t dst_y) {
+  for (std::uint16_t src_x = 0; src_x < src.get_height(); src_x++) {
+    for (std::uint16_t src_y = 0; src_y < src.get_width(); src_y++) {
+      dst.set_pixel(dst_x + src_x, dst_y + src_y, src.get_pixel(src_x, src_y));
+    }
+  }
+}
+
+#endif  // BLIT_HPP
