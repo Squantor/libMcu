db50a1739ab4e5e558f5807ab5c2a446fd2a0cec
Bart Bilos <squantmuts+git@gmail.com>
2026-08-28 11:25:48 +0200

    cleanup of 24XXX eeprom driver

--- inc/drivers/24xxx.hpp
+++ inc/drivers/24xxx.hpp
@@ -19,29 +19,30 @@ template <auto &i2c_hal, const libmcu::I2cDeviceAddress &i2c_address, auto &conf
           libmcu::Assert_concept assert_policy = libmcu::Assert_default>
 struct Eeprom_24xxx : public DriverBase, public libmcu::NonBlocking {
   /**
-   * @brief
+   * @brief Initializes the driver and checks if the device is present
    * @return constexpr libmcu::Results
    */
   constexpr libmcu::Results init() {
     state = libmcu::States::Initializing;
-    address_buffer.fill(0);
-    page_buffer.fill(0);
+    // Set internal EEPROM read/write address to zero
+    address_buffer[0] = 0;
     i2c_hal.Transmit(i2c_address, address_buffer);
-    i2c_hal.Receive(i2c_address, page_buffer, this);
     return libmcu::Results::NoError;
   }
-
-  constexpr void test(void) {
-    address_buffer[0] = 0;
-    for (std::size_t i = 0; i < page_buffer.size(); i++) {
-      page_buffer[i] = i;
-    }
-    i2c_hal.StartMasterTransmit(i2c_address, address_buffer);
-    i2c_hal.StopMasterTransmit(page_buffer, this);
-    state = libmcu::States::Busy;
-  }
-
+  /**
+   * @todo Reads data from the EEPROM
+   */
+  /**
+   * @todo Writes data to the EEPROM
+   */
+  /**
+   * @brief Progress function, not used
+   */
   constexpr void progress(void) final {}
+  /**
+   * @brief Callback used by I2C HAL
+   * @param result Transaction result
+   */
   constexpr void callback(libmcu::Results result) final {
     switch (state) {
       case libmcu::States::Initializing:
@@ -73,7 +74,6 @@ struct Eeprom_24xxx : public DriverBase, public libmcu::NonBlocking {
     }
   }
   std::array<std::uint8_t, config.address_size> address_buffer;  //< Buffer for an address
-  std::array<std::uint8_t, config.page_size> page_buffer;        //< Buffer for a page
 };
 }  // namespace libmcudrv::eeprom_24xxx
 
