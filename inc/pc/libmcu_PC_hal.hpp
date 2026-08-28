41a9bd45020f4a4835defb7341b763abddf137d8
Bart Bilos <squantmuts+git@gmail.com>
2026-08-28 14:25:44 +0200

    Added includes for PC unittests and simulations

---
+++ inc/pc/libmcu_PC_hal.hpp
@@ -0,0 +1,16 @@
+/**
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2026 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ *
+ * @file libmcu_PC_hal.hpp
+ * @brief libmcu HAL targeting PC targets, used for unittests and simulations
+ */
+#ifndef LIBMCU_PC_HAL_HPP
+#define LIBMCU_PC_HAL_HPP
+
+#include "libmcu_PC_ll.hpp"
+#include "../libmcu/libmcuhal.hpp"
+
+#endif
---
+++ inc/pc/libmcu_PC_ll.hpp
@@ -0,0 +1,15 @@
+/**
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2026 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ *
+ * @file libmcu_PC_LL.hpp
+ * @brief libmcu LL targeting PC targets, used for unittests and simulations
+ */
+#ifndef LIBMCU_PC_LL_HPP
+#define LIBMCU_PC_LL_HPP
+
+#include "../libmcu/libmcull.hpp"
+
+#endif
